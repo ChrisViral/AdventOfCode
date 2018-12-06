1f169cd904f63285407ecd337294cc3a27f8d4bf
Christophe Savard <christophe_savard@hotmail.ca>
2018-12-06 05:12:41 -0500

    Initial push with template

---
+++ Python/Template.pyi
@@ -0,0 +1,17 @@
+import sys
+from typing import List
+
+def main(args: List[str]):
+    """
+    Application entry point
+    :param args: Argument list, should contain the file to load
+    """
+
+    # File read stub
+    with open(args[0], "r") as f:
+        for line in f:
+            pass
+
+# Only run if entry point
+if __name__ == "__main__":
+    main(sys.argv)
