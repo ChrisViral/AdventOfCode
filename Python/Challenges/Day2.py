7b989e034a41daee4b704b6e294399c905c69ccf
Christophe Savard <christophe_savard@hotmail.ca>
2018-12-07 00:05:50 -0500

    Put back type annotations

--- Python/Challenges/Day2.py
+++ Python/Challenges/Day2.py
@@ -1,22 +1,23 @@
 import sys
+from typing import List, Set
 from collections import Counter
 
 
-def main(args):
+def main(args: List[str]) -> None:
     """
     Application entry point
     :param args: Argument list, should contain the file to load at index 1
     """
 
-    twos = 0
-    threes = 0
-    ids = []
+    twos: int = 0
+    threes: int = 0
+    ids: List[str] = []
 
     # Read file
     with open(args[1], "r") as f:
         for line in f:
             ids.append(line)
-            counts = set(Counter(line).values())
+            counts: Set[int] = set(Counter(line).values())
             if 2 in counts:
                 twos += 1
             if 3 in counts:
@@ -24,14 +25,19 @@ def main(args):
 
     print(f"Part one checksum: {twos * threes}")
 
+    # Annotate tuple unpacking variables
+    a: str
+    b: str
+
     # While the list has more than one element
     while len(ids) > 1:
         # Get last element, keep the rest for the secondary loop
-        first = ids.pop()
+        first: str = ids.pop()
         # Get second element
         for second in ids:
-            mismatch = False
-            diff = ""
+            mismatch: bool = False
+            diff: str = ""
+
             # Check character by character
             for a, b in zip(first, second):
                 # If mismatch
