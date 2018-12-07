a588670df29b86d77580ced286fbcc6e96af4cbf
Christophe Savard <christophe_savard@hotmail.ca>
2018-12-07 02:50:07 -0500

    Day 5 complete

--- Python/Challenges/Day5.py
+++ Python/Challenges/Day5.py
@@ -1,5 +1,6 @@
 import sys
-from typing import List
+import re
+from typing import List, Tuple
 
 
 def main(args: List[str]) -> None:
@@ -7,25 +8,75 @@ def main(args: List[str]) -> None:
     Application entry point
     :param args: Argument list, should contain the file to load at index 1
     """
+
     # Get polymer chain
-    polymer: str
     with open(args[1], "r") as f:
-        polymer = f.readline()
+        original: str = f.readline().strip()
 
-    reactions: int = 0
-    while react(polymer):
-        reactions += 1
+    # Fully react the polymer
+    polymer = fully_react(original)
+    minimum: int = len(polymer)
 
-    print(f"Part one polymer length: {len(polymer)}\nProduct found after {reactions} reactions")
+    # Print results
+    print(f"Part one polymer length: {minimum}")
+
+    # Try with each letter of the alphabet
+    for c in "abcdefghijklmnopqrstuvwxyz":
+        # Remove the letter from the original polymer, case insensitive, then test
+        polymer = re.sub(c, "", original, flags=re.IGNORECASE)
+        polymer = fully_react(polymer)
+        minimum = min(minimum, len(polymer))
+
+    # Print results
+    print(f"Part two polymer length: {minimum}")
 
 
-def react(polymer: str) -> bool:
+def fully_react(polymer: str) -> str:
+    """
+    Fully reacts a polymer until it cannot react further
+    :param polymer: Polymer chain to react
+    :return: The fully reacted polymer chain
+    """
+
+    # React as long as possible
+    reacting: bool = True
+    while reacting:
+        reacting, polymer = react(polymer)
+
+    # When done, return the result
+    return polymer
+
+
+def react(polymer: str) -> Tuple[bool, str]:
+    """
+    Reacts the polymer string and removes the next polymer reaction if any is found
+    :param polymer: Polymer string to react
+    :return: True if a reaction occurred, false otherwise
+    """
+
     # Can only react if there is 2 or more components
     if len(polymer) < 2:
-        return False
+        return False, polymer
 
-    prev: chr = polymer[0]
-    return False
+    # Previous/Current character
+    prev: str = polymer[0]
+    curr: str
+
+    # Loop through all the remaining characters
+    for curr in polymer[1:]:
+        # If the same character and case is different
+        if prev.lower() == curr.lower() and prev.islower() != curr.islower():
+            # Remove the character pair from the string
+            polymer = polymer.replace(prev + curr, "")
+            break
+        # If no reaction, set current character as previous
+        prev = curr
+    else:
+        # If completed normally, no replacements have been done
+        return False, polymer
+
+    # If broke out, More replacements might need to be done
+    return True, polymer
 
 
 # Only run if entry point
