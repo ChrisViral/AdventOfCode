e82cbc02c118848f29cddd03eaee730785348f71
Christophe Savard <christophe_savard@hotmail.ca>
2018-12-08 03:29:58 -0500

    Move around C# solutions

---
+++ CSharp/Challenge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Challenge base class
+    /// </summary>
+    public abstract class Challenge
+    {
+        #region Properties
+        /// <summary>
+        /// ID of the challenge
+        /// </summary>
+        public abstract int ID { get; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Gets all the input lines for this given challenge
+        /// </summary>
+        /// <returns>Enumerable of all lines</returns>
+        protected IEnumerable<string> GetLines()
+        {
+            using (StreamReader fs = File.OpenText($"Input/day{this.ID}.txt"))
+            {
+                for (string line = fs.ReadLine(); line != null; line = fs.ReadLine())
+                {
+                    yield return line.Trim();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints the given message to the Console output
+        /// </summary>
+        /// <param name="message"></param>
+        protected void Print(string message) => Console.WriteLine(message);
+
+        /// <summary>
+        /// Solving method of the challenge
+        /// </summary>
+        public abstract void Solve();
+        #endregion
+    }
+}
---
+++ CSharp/Challenges/Day1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Challenges
+{
+    /// <summary>
+    /// Day 1 Part 1 challenge
+    /// </summary>
+    public sealed class Day1 : Challenge
+    {
+        #region Properties
+        /// <summary>
+        /// Challenge ID
+        /// </summary>
+        public override int ID { get; } = 1;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Solver
+        /// </summary>
+        public override void Solve()
+        {
+            List<int> jumps = new List<int>();
+            HashSet<int> frequencies = new HashSet<int>();
+            int frequency = 0;
+            foreach (int jump in GetLines().Select(int.Parse))
+            {
+                jumps.Add(jump);
+                frequency += jump;
+                frequencies.Add(frequency);
+            }
+
+            Print("Part one frequency: " + frequency);
+
+            bool found = false;
+            while (!found)
+            {
+                foreach (int jump in jumps)
+                {
+                    frequency += jump;
+                    if (!frequencies.Add(frequency))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            Print("Part two repeated: " + frequency);
+        }
+        #endregion
+    }
+}
