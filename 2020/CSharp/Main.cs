151b44e899671ff6257fd958f5701fa92bf872a8
Christophe Savard <christophe_savard@hotmail.ca>
2020-12-05 01:12:23 -0500

    Did someone say C# 9.0?

--- 2020/CSharp/Main.cs
+++ 2020/CSharp/Main.cs
@@ -1,132 +1,100 @@
-﻿#nullable enable
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using AdventOfCode.Solvers.Base;
 
-
-
-namespace AdventOfCode
+//Check for arguments
+if (args.Length == 0)
 {
-    /// <summary>
-    /// Entry point class
-    /// </summary>
-    internal static class Program
+    Exit("No day specified", 1);
+}
+
+//Make sure the file exists
+string day = args[0];
+FileInfo inputFile = new ($@"Input\{day.ToLower()}.txt");
+if (!inputFile.Exists)
+{
+    Exit($"Input file {day}.txt does not exist.", 1);
+    return;
+}
+
+ISolver solver;
+try
+{
+    //Helpful types
+    Type   solverInterfaceType   = typeof(ISolver);
+    Type   baseSolverType        = typeof(Solver);
+    Type[] constructorParamTypes = { typeof(FileInfo) };
+    
+    //Making sure our solver types are valid
+    Debug.Assert(solverInterfaceType.IsAssignableFrom(baseSolverType), $"{baseSolverType} does not inherit from {solverInterfaceType}");
+
+    //Get solver types 
+    Type? solverType = Assembly.GetCallingAssembly()
+                               .GetTypes()
+                               .Where(t => !t.IsAbstract
+                                        && !t.IsGenericType
+                                        && t.IsAssignableTo(baseSolverType)
+                                        && t.GetConstructor(constructorParamTypes) is not null)
+                               .SingleOrDefault(t => t.Name == day);
+
+    //Make sure the type exists
+    if (solverType is null)
     {
-        #region Constants
-        //Helpful types
-        private static readonly Type   solverInterfaceType   = typeof(ISolver);
-        private static readonly Type   baseSolverType        = typeof(Solver);
-        private static readonly Type[] constructorParamTypes = { typeof(FileInfo) };
-        #endregion
-
-        /// <summary>
-        /// Program entry point
-        /// </summary>
-        /// <param name="args">Program arguments</param>
-        private static void Main(string[] args)
-        {
-            //Check for arguments
-            if (args.Length == 0)
-            {
-                Exit("No day specified", 1);
-            }
-
-            //Make sure the file exists
-            string day = args[0];
-            FileInfo inputFile = new FileInfo($@"Input\{day.ToLower()}.txt");
-            if (!inputFile.Exists)
-            {
-                Exit($"Input file {day}.txt does not exist.", 1);
-                return;
-            }
-
-            ISolver solver;
-            try
-            {
-                //Making sure our solver types are valid
-                Debug.Assert(solverInterfaceType.IsAssignableFrom(baseSolverType), $"{baseSolverType} does not inherit from {solverInterfaceType}");
-
-                //Get solver types 
-                Type? solverType = Assembly.GetCallingAssembly()
-                                           .GetTypes()
-                                           .Where(t => !t.IsAbstract
-                                                    && !t.IsGenericType
-                                                    && t.IsAssignableTo(baseSolverType)
-                                                    && t.GetConstructor(constructorParamTypes) is not null)
-                                           .SingleOrDefault(t => t.Name == day);
-
-                //Make sure the type exists
-                if (solverType is null)
-                {
-                    Exit($"Could not find a matching Solver for {day}", 1);
-                    return;
-                }
-
-                //Instantiate the solver
-                solver = (ISolver)Activator.CreateInstance(solverType, inputFile)!; //Throw if cast fails
-            }
-            catch (Exception e)
-            {
-                //If any exception happens, immediately hop out
-                Exit($"Exception while creating solver for {day}", e);
-                return;
-            }
-
-            //Setup trace file
-            #if DEBUG
-            using TextWriterTraceListener textListener = new TextWriterTraceListener(File.CreateText(@"..\..\..\results.txt"));
-            #else
-            using TextWriterTraceListener textListener = new TextWriterTraceListener(File.CreateText("results.txt"));
-            #endif
-            Trace.Listeners.Add(textListener);
-            using ConsoleTraceListener consoleListener = new ConsoleTraceListener();
-            Trace.Listeners.Add(consoleListener);
-            Trace.AutoFlush = true;
-
-            try
-            {
-                //Run solver
-                solver.Run();
-            }
-            catch (Exception e)
-            {
-                //Log any exceptions that occur
-                Exit($"Exception while running solver {solver.GetType().Name}", e);
-                return;
-            }
-
-            //Cleanup and exit
-            Trace.Close();
-            Exit();
-        }
-        
-        #region Static methods
-        /// <summary>
-        /// Exits while logging an exception
-        /// </summary>
-        /// <param name="message">Message to log</param>
-        /// <param name="e">Exception to log</param>
-        private static void Exit(string message, Exception e) => Exit($"{message}\n[{e.GetType().Name}]: {e.Message}\n{e.StackTrace}\n", 1);
-
-        /// <summary>
-        /// Exits the program, while logging an optional message
-        /// </summary>
-        /// <param name="message">Message to log</param>
-        /// <param name="exitCode">Program exit code</param>
-        private static void Exit(string? message = null, int exitCode = 0)
-        {
-            //Log message if any
-            if (!string.IsNullOrEmpty(message))
-            {
-                Console.WriteLine(message);
-            }
-
-            //Exit
-            Environment.Exit(exitCode);
-        }
-        #endregion
+        Exit($"Could not find a matching Solver for {day}", 1);
+        return;
     }
+
+    //Instantiate the solver
+    solver = (ISolver)Activator.CreateInstance(solverType, inputFile)!; //Throw if cast fails
+}
+catch (Exception e)
+{
+    //If any exception happens, immediately hop out
+    ExitOnException($"Exception while creating solver for {day}", e);
+    return;
+}
+
+//Setup trace file
+#if DEBUG
+using TextWriterTraceListener textListener = new (File.CreateText(@"..\..\..\results.txt"));
+#else
+using TextWriterTraceListener textListener = new (File.CreateText("results.txt"));
+#endif
+Trace.Listeners.Add(textListener);
+using ConsoleTraceListener consoleListener = new ();
+Trace.Listeners.Add(consoleListener);
+Trace.AutoFlush = true;
+
+try
+{
+    //Run solver
+    solver.Run();
+}
+catch (Exception e)
+{
+    //Log any exceptions that occur
+    ExitOnException($"Exception while running solver {solver.GetType().Name}", e);
+    return;
+}
+
+//Cleanup and exit
+Trace.Close();
+Exit();
+
+
+static void ExitOnException(string message, Exception e) => Exit($"{message}\n[{e.GetType().Name}]: {e.Message}\n{e.StackTrace}\n", 1);
+
+static void Exit(string? message = null, int exitCode = 0)
+{
+    //Log message if any
+    if (!string.IsNullOrEmpty(message))
+    {
+        Console.WriteLine(message);
+    }
+
+    //Exit
+    Environment.Exit(exitCode);
 }
