211eaf2df82d11a1e77af64efe803ae796e7702b
Christophe Savard <christophe_savard@hotmail.ca>
2020-12-06 04:09:54 -0500

    Add Day 4

---
+++ 2020/CSharp/Solvers/Day4.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Solvers
+{
+    public class Day4 : Solver<Day4.Passport[]>
+    {
+        public class Passport
+        {
+            private const RegexOptions OPTIONS = RegexOptions.Compiled | RegexOptions.Singleline;
+            private static readonly Regex heightMatch = new(@"^(\d{2,3})(cm|in)$", OPTIONS);
+            private static readonly Regex hairMatch   = new(@"^#[\da-f]{6}$", OPTIONS);
+            private static readonly Regex idMatch     = new(@"^\d{9}$", OPTIONS);
+            private static readonly HashSet<string> validEyeColours = new() { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            
+            public string? byr;
+            public string? iyr;
+            public string? eyr;
+            public string? hgt;
+            public string? hcl;
+            public string? ecl;
+            public string? pid;
+            
+            public bool IsValid => this.byr is not null
+                                && this.iyr is not null
+                                && this.eyr is not null
+                                && this.hgt is not null
+                                && this.hcl is not null
+                                && this.ecl is not null
+                                && this.pid is not null;
+
+            public bool Validate()
+            {
+                if (!int.TryParse(this.byr, out int birthYear) || birthYear is < 1920 or > 2002) return false;
+                if (!int.TryParse(this.iyr, out int issueYear) || issueYear is < 2010 or > 2020) return false;
+                if (!int.TryParse(this.eyr, out int expYear)   || expYear   is < 2020 or > 2030) return false;
+
+                Match match = heightMatch.Match(this.hgt!);
+                if (!match.Success || match.Groups.Count != 3) return false;
+                if (!int.TryParse(match.Groups[1].Value, out int height)) return false;
+                switch (match.Groups[2].Value)
+                {
+                    case "cm":
+                        if (height is < 150 or > 193) return false;
+                        break;
+                    case "in":
+                        if (height is < 59 or > 76) return false;
+                        break;
+                    
+                    default:
+                        return false;
+                }
+
+                return hairMatch.IsMatch(this.hcl!) && idMatch.IsMatch(this.pid!) && validEyeColours.Contains(this.ecl!);
+            }
+        }
+
+        private const string PATTERN = "([a-z]{3}):([a-z0-9#]+)";
+
+        /// <summary>
+        /// Creates a new generic <see cref="Solver{T}"/> with the input data properly parsed
+        /// </summary>
+        /// <param name="file">Input file</param>
+        /// <param name="splitters">Splitting characters, defaults to newline only</param>
+        /// <param name="options">Input parsing options, defaults to removing empty entries and trimming entries</param>
+        /// <exception cref="ArgumentException">Thrown if the <paramref name="file"/> does not exist or has an invalid extension</exception>
+        /// <exception cref="FileLoadException">Thrown if the input <paramref name="file"/> could not be properly loaded</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the conversion to <typeparamref name="T"/> fails</exception>
+        public Day4(FileInfo file) : base(file, options: StringSplitOptions.TrimEntries) { }
+
+        /// <inheritdoc cref="Solver"/>
+        public override void Run()
+        {
+            Passport[] valid = this.Input.Where(p => p.IsValid).ToArray();
+            Trace.WriteLine(valid.Length);
+            Trace.WriteLine(valid.Count(p => p.Validate()));
+
+        }
+
+        /// <inheritdoc cref="Solver{T}"/>
+        public override Passport[] Convert(string[] rawInput)
+        {
+            List<string> input = new();
+            string line = string.Empty;
+            foreach (string inputLine in rawInput)
+            {
+                if (string.IsNullOrWhiteSpace(inputLine))
+                {
+                    input.Add(line);
+                    line = string.Empty;
+                }
+                else
+                {
+                    line += " " + inputLine;
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                input.Add(line);
+            }
+            
+            return RegexUtils.PopulateObjects<Passport>(PATTERN, input, RegexOptions.Compiled);
+        }
+    }
+}
--- 2020/CSharp/Utils/RegexUtils.cs
+++ 2020/CSharp/Utils/RegexUtils.cs
@@ -17,24 +17,25 @@ namespace AdventOfCode.Utils
         
         #region Static methods
         /// <summary>
-        /// Creates a <typeparamref name="T"/> object from a <see cref="Regex"/> match<p/>
-        /// In order for the conversion to work, the type must have a constructor with the same amount of parameters as there are captures.
-        /// Additionally, all the parameter types must implement <see cref="IConvertible"/>
+        /// Constructs a <typeparamref name="T"/> object from a <see cref="Regex"/> match<br/>
+        /// The construction finds a constructor on the type with the same amount of parameters as there are captures,<br/>
+        /// then populates the parameters by converting the captures to the parameter type.<br/>
+        /// Additionally, all the parameter types must implement <see cref="IConvertible"/>.
         /// </summary>
         /// <typeparam name="T">Type of object to create</typeparam>
         /// <param name="pattern">Regex pattern</param>
         /// <param name="input">Input strings</param>
-        /// <param name="compile">If the <see cref="Regex"/> should be compiled or not, defaults to false</param>
-        /// <returns>An array of the parsed <typeparamref name="T"/> objects</returns>
-        /// <exception cref="ArgumentException">If the pattern string is invalid, or if no matching constructurs of <typeparamref name="T"/> were found</exception>
+        /// <param name="options">The applied Regex options, defaults to <see cref="RegexOptions.None"/></param>
+        /// <returns>An array of the created <typeparamref name="T"/> objects</returns>
+        /// <exception cref="ArgumentException">If the pattern string is invalid, or if no matching constructors of <typeparamref name="T"/> were found</exception>
         /// <exception cref="InvalidCastException">If an error happens while casting the parameters</exception>
-        public static T[] CreateObjects<T>(string pattern, string[] input, bool compile = false)
+        public static T[] ConstructObjects<T>(string pattern, IReadOnlyList<string> input, RegexOptions options = RegexOptions.None) where T : class
         {
             if (string.IsNullOrWhiteSpace(pattern)) throw new ArgumentException("Cannot have a null or whitespace pattern string", nameof(pattern));
-            if (input.Length == 0) return Array.Empty<T>();
+            if (input.Count == 0) return Array.Empty<T>();
             
             //Create match
-            Regex match = new(pattern, compile ? RegexOptions.Compiled : RegexOptions.None);
+            Regex match = new(pattern, options);
             
             //Get captures and matching Constructor
             string[] captures = GetCaptures(input[0], match);
@@ -44,15 +45,16 @@ namespace AdventOfCode.Utils
             
             //Get parameters
             ParameterInfo[] paramsInfo = constructor.GetParameters();
-            if (paramsInfo.Any(p => !convertibleType.IsAssignableFrom(p.ParameterType))) throw new ArgumentException($"Matching constructor for type {typeof(T)} has parameters which do not implement IConvertible", nameof(T));
+            if (paramsInfo.Any(p => !convertibleType.IsAssignableFrom(p.ParameterType)
+                                 || !convertibleType.IsAssignableFrom(Nullable.GetUnderlyingType(p.ParameterType)))) throw new ArgumentException($"Matching constructor for type {typeof(T)} has parameters which do not implement IConvertible", nameof(T));
 
             //Parse results
-            T[] results = new T[input.Length];
+            T[] results = new T[input.Count];
             object[] parameters = new object[paramsInfo.Length];
-            results[0] = ParseLine<T>(captures, parameters, constructor, paramsInfo);
-            for (int i = 1; i < input.Length; i++)
+            results[0] = CreateObject<T>(captures, parameters, constructor, paramsInfo);
+            for (int i = 1; i < input.Count; i++)
             {
-                results[i] = ParseLine<T>(GetCaptures(input[i], match), parameters, constructor, paramsInfo);
+                results[i] = CreateObject<T>(GetCaptures(input[i], match), parameters, constructor, paramsInfo);
             }
 
             return results;
@@ -79,14 +81,73 @@ namespace AdventOfCode.Utils
         /// <param name="paramsInfo">Parameter signatures</param>
         /// <returns>The parsed and created <typeparamref name="T"/> object</returns>
         /// <exception cref="InvalidCastException">If the conversion to a parameter failed</exception>
-        private static T ParseLine<T>(IReadOnlyList<string> captures, object[] parameterCache, ConstructorInfo constructor, IReadOnlyList<ParameterInfo> paramsInfo)
+        private static T CreateObject<T>(IReadOnlyList<string> captures, object[] parameterCache, ConstructorInfo constructor, IReadOnlyList<ParameterInfo> paramsInfo)
         {
             for (int i = 0; i < paramsInfo.Count; i++)
             {
-                parameterCache[i] = Convert.ChangeType(captures[i], paramsInfo[i].ParameterType) ?? throw new InvalidCastException($"Could not convert {captures[i]} to {paramsInfo[i].ParameterType}");
+                //Get the underlying type if a nullable
+                Type paramType = paramsInfo[i].ParameterType;
+                Type type = Nullable.GetUnderlyingType(paramType) ?? paramType;
+                //Create and set the value
+                parameterCache[i] = Convert.ChangeType(captures[i], type) ?? throw new InvalidCastException($"Could not convert {captures[i]} to {type}");
             }
 
             return (T)constructor.Invoke(parameterCache);
+        }
+
+        /// <summary>
+        /// Populates a <typeparamref name="T"/> object from a <see cref="Regex"/> match<br/>
+        /// To populate, all matches from the regex are found in the input string, then are separated<br/>
+        /// into key/value pairs if there are exactly two captures. The value is then applied to the public field matched with the key.<br/>
+        /// Additionally, all the field types must implement <see cref="IConvertible"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of object to populate</typeparam>
+        /// <param name="pattern">Regex pattern</param>
+        /// <param name="input">Input strings</param>
+        /// <param name="options">The applied Regex options, defaults to <see cref="RegexOptions.None"/></param>
+        /// <returns>An array of the populated <typeparamref name="T"/> objects</returns>
+        /// <exception cref="ArgumentException">If the pattern string is invalid, or if no matching constructors of <typeparamref name="T"/> were found</exception>
+        /// <exception cref="InvalidCastException">If an error happens while casting the parameters</exception>
+        public static T[] PopulateObjects<T>(string pattern, IReadOnlyList<string> input, RegexOptions options = RegexOptions.None) where T : class, new()
+        {
+            if (string.IsNullOrWhiteSpace(pattern)) throw new ArgumentException("Cannot have a null or whitespace pattern string", nameof(pattern));
+            if (input.Count == 0) return Array.Empty<T>();
+            
+            //Create match
+            Regex match = new(pattern, options);
+            T[] results = new T[input.Count];
+            Dictionary<string, FieldInfo> fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance)
+                                                            .Where(f => convertibleType.IsAssignableFrom(f.FieldType)
+                                                                     || convertibleType.IsAssignableFrom(Nullable.GetUnderlyingType(f.FieldType)))
+                                                            .ToDictionary(f => f.Name, f => f);
+            for (int i = 0; i < input.Count; i++)
+            {
+                //Find all matches, extract key/value pairs
+                (string, string)[] matches = match.Matches(input[i])
+                                                  .Select(m => m.Groups.Cast<Group>().Skip(1).ToArray())
+                                                  .Where(a => a.Length is 2)
+                                                  .Select(a => (a[0].Value, a[1].Value))
+                                                  .ToArray();
+                //Create object and populate
+                T obj = Activator.CreateInstance<T>();
+                foreach ((string key, string value) in matches)
+                {
+                    //If the key matches to a field
+                    if (fields.TryGetValue(key, out FieldInfo? field))
+                    {
+                        //Get the underlying type if a nullable
+                        Type fieldType = Nullable.GetUnderlyingType(field.FieldType) ?? field.FieldType;
+                        //Create and set the value
+                        object result = Convert.ChangeType(value, fieldType) ?? throw new InvalidCastException($"Could not convert {value} to {field.FieldType}");
+                        field.SetValue(obj, result);
+                    }
+                }
+                
+                //Set the object
+                results[i] = obj;
+            }
+
+            return results;
         }
         #endregion
     }
