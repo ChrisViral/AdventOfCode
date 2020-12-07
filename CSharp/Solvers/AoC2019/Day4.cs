0a02fc8d3ba06c9e66dc91edef3c268df17012d1
Christophe Savard <christophe_savard@hotmail.ca>
2020-12-07 05:10:33 -0500

    2019 Day 4

---
+++ CSharp/Solvers/AoC2019/Day4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Solvers.AoC2019
+{
+    /// <summary>
+    /// Solver for 2019 Day 4
+    /// </summary>
+    public class Day4 : Solver<Range>
+    {
+        #region Constants
+        private static readonly Regex adjacentMatch = new(@"^\d*(\d)\1+\d*$", RegexOptions.Compiled);
+        private static readonly Regex adjacentPairMatch = new(@"(?:^|(\d)(?!\1))(\d)\2(?!\2)", RegexOptions.Compiled);
+        private static readonly Regex increasingMatch = new(@"^1*2*3*4*5*6*7*8*9*$", RegexOptions.Compiled);
+        #endregion
+        
+        #region Constructors
+        /// <summary>
+        /// Creates a new <see cref="Day4"/> Solver with the input data properly parsed
+        /// </summary>
+        /// <param name="file">Input file</param>
+        /// <exception cref="ArgumentException">Thrown if the <paramref name="file"/> does not exist or has an invalid extension</exception>
+        /// <exception cref="FileLoadException">Thrown if the input <paramref name="file"/> could not be properly loaded</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="T"/> fails</exception>
+        public Day4(FileInfo file) : base(file) { }
+        #endregion
+
+        #region Methods
+        /// <inheritdoc cref="Solver"/>
+        public override void Run()
+        {
+            List<string> valid = this.Input.AsEnumerable()
+                                  .Select(p => p.ToString())
+                                  .Where(s => adjacentMatch.IsMatch(s) && increasingMatch.IsMatch(s))
+                                  .ToList();
+            AoCUtils.LogPart1(valid.Count);
+            AoCUtils.LogPart1(valid.Count(s => adjacentPairMatch.IsMatch(s)));
+        }
+
+        /// <inheritdoc cref="Solver{T}"/>
+        public override Range Convert(string[] rawInput)
+        {
+            string[] splits = rawInput[0].Split('-', StringSplitOptions.TrimEntries);
+            return new Range(int.Parse(splits[0]), int.Parse(splits[1]));
+        }
+        #endregion
+    }
+}
--- CSharp/Utils/AoCUtils.cs
+++ CSharp/Utils/AoCUtils.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace AdventOfCode.Utils
 {
@@ -50,6 +52,28 @@ namespace AdventOfCode.Utils
         /// </summary>
         /// <param name="answer">Answer to log</param>
         public static void LogPart2(object answer) => Trace.WriteLine($"Part 2: {answer}");
+        #endregion
+        
+        #region Extension methods
+        /// <summary>
+        /// Transforms the range into an enumerable over it's start and end
+        /// </summary>
+        /// <returns>An enumerable over the entire Range</returns>
+        /// <exception cref="ArgumentException">If any of the indices are marked as from the end</exception>
+        public static IEnumerable<int> AsEnumerable(this Range range)
+        {
+            //Make sure the indices aren't from the end
+            if (range.Start.IsFromEnd || range.End.IsFromEnd) throw new ArgumentException("Range indices cannot be from end for enumeration", nameof(range));
+            
+            return range.End.Value > range.Start.Value ? Enumerable.Range(range.Start.Value, range.End.Value - range.Start.Value - 1) : Enumerable.Range(0, 0);
+        }
+
+        /// <summary>
+        /// Range GetEnumerator method, allows foreach over a range as long as the indices are not from the end
+        /// </summary>
+        /// <returns>An enumerator over the entire range</returns>
+        /// <exception cref="ArgumentException">If any of the indices are marked as from the end</exception>
+        public static IEnumerator<int> GetEnumerator(this Range range) => range.AsEnumerable().GetEnumerator();
         #endregion
     }
 }
