31f644a7ec2dcff4bf0b09c1abfbcefc19434d74
Christophe Savard <christophe_savard@hotmail.ca>
2020-12-07 00:33:52 -0500

    Move Operations to their own class

--- CSharp/Intcode/IntcodeVM.cs
+++ CSharp/Intcode/IntcodeVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using Operation = AdventOfCode.Intcode.Operations.Operation;
 
 namespace AdventOfCode.Intcode
 {
@@ -8,27 +9,16 @@ namespace AdventOfCode.Intcode
     /// </summary>
     public class IntcodeVM
     {
-        /// <summary>
-        /// Intcode Opcodes
-        /// </summary>
-        public enum Opcodes
-        {
-            ADD = 1,
-            MUL = 2,
-            
-            NOP = 0,
-            HLT = 99
-        }
-
-        /// <summary>
-        /// Intcode operation delegate
-        /// </summary>
-        /// <param name="memory">Memory of the VM</param>
-        /// <param name="pointer">Pointer of the VM</param>
-        public delegate void Operation(int[] memory, ref int pointer);
-
         #region Constants
-        private const int HALT = -1;
+        /// <summary>
+        /// Halted pointer state
+        /// </summary>
+        public const int HALT = -1;
+        /// <summary>
+        /// Default pointer state
+        /// </summary>
+        public const int DEFAULT = 0;
+        
         private const StringSplitOptions OPTIONS = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
         private static readonly char[] splitters = { ',' };
         #endregion
@@ -37,7 +27,7 @@ namespace AdventOfCode.Intcode
         /// <summary>
         /// Intcode program memory pointer
         /// </summary>
-        private int pointer;
+        private int pointer = DEFAULT;
         /// <summary>
         /// Intcode program memory
         /// </summary>
@@ -89,9 +79,9 @@ namespace AdventOfCode.Intcode
             //Program loop
             while (this.pointer is not HALT)
             {
-                int op = this.Fetch;                        //Fetch
-                Operation operation = Decode(op);           //Decode
-                operation(this.memory, ref this.pointer);   //Execute
+                int op = this.Fetch;                         //Fetch
+                Operation operation = Operations.Decode(op); //Decode
+                operation(this.memory, ref this.pointer);    //Execute
             }
 
             //Use the address currently stored in result to store the out value
@@ -126,67 +116,9 @@ namespace AdventOfCode.Intcode
         /// </summary>
         public void Reset()
         {
-            this.pointer = 0;
+            this.pointer = DEFAULT;
             this.originalState.CopyTo(this.memory);
         }
-        #endregion
-        
-        #region Static methods
-        /// <summary>
-        /// Decodes an opcode into it's associated operation
-        /// </summary>
-        /// <param name="opcode">Opcode to decode</param>
-        /// <returns>The Operation this Opcode refers to</returns>
-        /// <exception cref="InvalidEnumArgumentException">If an Invalid Opcode is detected</exception>
-        private static Operation Decode(int opcode)
-        {
-            return (Opcodes)opcode switch
-            {
-                Opcodes.NOP => Nop,
-                Opcodes.ADD => Add,
-                Opcodes.MUL => Mul,
-                Opcodes.HLT => Hlt,
-                _           => throw new InvalidEnumArgumentException(nameof(opcode), opcode, typeof(Opcodes))
-            };
-        }
-        #endregion
-        
-        #region Operations
-        /// <summary>
-        /// NOP Operation, increments pointer only
-        /// </summary>
-        /// <param name="memory">Current VM memory</param>
-        /// <param name="pointer">Current VM pointer</param>
-        private static void Nop(int[] memory, ref int pointer) => pointer++;
-        
-        /// <summary>
-        /// ADD Operation, adds the values in the addresses of the first and second arguments into the address of the third argument
-        /// </summary>
-        /// <param name="memory">Current VM memory</param>
-        /// <param name="pointer">Current VM pointer</param>
-        private static void Add(int[] memory, ref int pointer)
-        {
-            memory[pointer + 3] = memory[pointer + 1] + memory[pointer + 2];
-            pointer += 4;
-        }
-
-        /// <summary>
-        /// MUL Operation, multiplies the values in the addresses of the first and second arguments into the address of the third argument
-        /// </summary>
-        /// <param name="memory">Current VM memory</param>
-        /// <param name="pointer">Current VM pointer</param>
-        private static void Mul(int[] memory, ref int pointer)
-        {
-            memory[pointer + 3] = memory[pointer + 1] * memory[pointer + 2];
-            pointer += 4;
-        }
-
-        /// <summary>
-        /// HLT Operation, sets the pointer into the halted state
-        /// </summary>
-        /// <param name="memory">Current VM memory</param>
-        /// <param name="pointer">Current VM pointer</param>
-        private static void Hlt(int[] memory, ref int pointer) => pointer = HALT;
         #endregion
     }
 }
---
+++ CSharp/Intcode/Operations.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+
+namespace AdventOfCode.Intcode
+{
+    /// <summary>
+    /// Intcode Operations
+    /// </summary>
+    public static class Operations
+    {
+        /// <summary>
+        /// Intcode Opcodes
+        /// </summary>
+        public enum Opcodes
+        {
+            ADD = 1,
+            MUL = 2,
+            
+            NOP = 0,
+            HLT = 99
+        }
+
+        /// <summary>
+        /// Intcode operation delegate
+        /// </summary>
+        /// <param name="memory">Memory of the VM</param>
+        /// <param name="pointer">Pointer of the VM</param>
+        public delegate void Operation(int[] memory, ref int pointer);
+        
+        #region Static methods
+        /// <summary>
+        /// Decodes an opcode into it's associated operation
+        /// </summary>
+        /// <param name="opcode">Opcode to decode</param>
+        /// <returns>The Operation this Opcode refers to</returns>
+        /// <exception cref="InvalidEnumArgumentException">If an Invalid Opcode is detected</exception>
+        public static Operation Decode(int opcode)
+        {
+            return (Opcodes)opcode switch
+            {
+                Opcodes.NOP => Nop,
+                Opcodes.ADD => Add,
+                Opcodes.MUL => Mul,
+                Opcodes.HLT => Hlt,
+                _           => throw new InvalidEnumArgumentException(nameof(opcode), opcode, typeof(Opcodes))
+            };
+        }
+        
+        /// <summary>
+        /// NOP Operation, increments pointer only
+        /// </summary>
+        /// <param name="memory">Current VM memory</param>
+        /// <param name="pointer">Current VM pointer</param>
+        private static void Nop(int[] memory, ref int pointer) => pointer++;
+        
+        /// <summary>
+        /// ADD Operation, adds the values in the addresses of the first and second arguments into the address of the third argument
+        /// </summary>
+        /// <param name="memory">Current VM memory</param>
+        /// <param name="pointer">Current VM pointer</param>
+        private static void Add(int[] memory, ref int pointer)
+        {
+            memory[pointer + 3] = memory[pointer + 1] + memory[pointer + 2];
+            pointer += 4;
+        }
+
+        /// <summary>
+        /// MUL Operation, multiplies the values in the addresses of the first and second arguments into the address of the third argument
+        /// </summary>
+        /// <param name="memory">Current VM memory</param>
+        /// <param name="pointer">Current VM pointer</param>
+        private static void Mul(int[] memory, ref int pointer)
+        {
+            memory[pointer + 3] = memory[pointer + 1] * memory[pointer + 2];
+            pointer += 4;
+        }
+
+        /// <summary>
+        /// HLT Operation, sets the pointer into the halted state
+        /// </summary>
+        /// <param name="memory">Current VM memory</param>
+        /// <param name="pointer">Current VM pointer</param>
+        private static void Hlt(int[] memory, ref int pointer) => pointer = IntcodeVM.HALT;
+        #endregion
+    }
+}
