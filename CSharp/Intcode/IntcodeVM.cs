4530937adeb317862aeeb8f6a03fb6dc4d02a272
Christophe Savard <christophe_savard@hotmail.ca>
2020-12-07 22:44:10 -0500

    Update VM I/O

--- CSharp/Intcode/IntcodeVM.cs
+++ CSharp/Intcode/IntcodeVM.cs
@@ -48,6 +48,10 @@ namespace AdventOfCode.Intcode
         /// Default pointer state
         /// </summary>
         public const int DEFAULT = 0;
+        /// <summary>
+        /// Default output stream size (1kb)
+        /// </summary>
+        public const int DEFAULT_SIZE = 1024;
         
         private const StringSplitOptions OPTIONS = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
         private static readonly char[] splitters = { ',' };
@@ -61,13 +65,13 @@ namespace AdventOfCode.Intcode
         /// <summary>Original memory state of the program</summary>
         private readonly ReadOnlyMemory<int> originalState;
         /// <summary>The input stream of the IntcodeVM</summary>
-        private readonly MemoryStream? inputStream;
-        /// <summary>The output stream of the IntcodeVM</summary>
-        private readonly MemoryStream? outputStream;
+        private readonly MemoryStream inputStream;
         /// <summary>The input stream reader of the IntcodeVM</summary>
-        private readonly BinaryReader? inputReader;
+        private readonly BinaryReader inputReader;
+        /// <summary>The output stream of the IntcodeVM</summary>
+        private readonly MemoryStream outputStream;
         /// <summary>The output stream writer of the IntcodeVM</summary>
-        private readonly BinaryWriter? outputWriter;
+        private readonly BinaryWriter outputWriter;
         /// <summary>Data relating to the VM</summary>
         private readonly VMData data;
         #endregion
@@ -91,30 +95,50 @@ namespace AdventOfCode.Intcode
 
         #region Constructors
         /// <summary>
-        /// Creates a new Intcode VM by parsing the given code
+        /// Creates a new Intcode VM by parsing the given code, and with an empty input buffer and resizable output buffer
+        /// </summary>
+        /// <param name="code">Comma separated Intcode to parse</param>
+        public IntcodeVM(string code) : this(code, new MemoryStream(), new MemoryStream(DEFAULT_SIZE)) { }
+        
+        /// <summary>
+        /// Creates a new Intcode VM by parsing the given code, and with the specified combined input and output array
+        /// </summary>
+        /// <param name="code">Comma separated Intcode to parse</param>
+        /// <param name="inputOutput">Combined input and output array</param>
+        public IntcodeVM(string code, int[] inputOutput) : this(code, GetBuffer(inputOutput)) { }
+        
+        /// <summary>
+        /// Creates a new Intcode VM by parsing the given code, and with the specified combined input and output buffer
+        /// </summary>
+        /// <param name="code">Comma separated Intcode to parse</param>
+        /// <param name="inputOutputBuffer">Combined input and output buffer</param>
+        public IntcodeVM(string code, byte[] inputOutputBuffer) : this(code, inputOutputBuffer, inputOutputBuffer) { }
+        
+        /// <summary>
+        /// Creates a new Intcode VM by parsing the given code, and with the specified byte arrays as input and output buffers
+        /// </summary>
+        /// <param name="code">Comma separated Intcode to parse</param>
+        /// <param name="input">Input buffer</param>
+        /// <param name="output">Output buffer</param>
+        public IntcodeVM(string code, byte[] input, byte[] output) : this(code, new MemoryStream(input), new MemoryStream(output)) { }
+        
+        /// <summary>
+        /// Creates a new Intcode VM by parsing the given code, and with the specified input and output MemoryStreams
         /// </summary>
         /// <param name="code">Comma separated Intcode to parse</param>
         /// <param name="inputStream">The input Stream for this Intcode VM</param>
         /// <param name="outputStream">The output Steam for this Intcode VM</param>
-        public IntcodeVM(string code, MemoryStream? inputStream = null, MemoryStream? outputStream = null)
+        public IntcodeVM(string code, MemoryStream inputStream, MemoryStream outputStream)
         {
             this.originalState = Array.ConvertAll(code.Split(splitters, OPTIONS), int.Parse);
             this.memory = new int[this.originalState.Length];
             this.originalState.CopyTo(this.memory);
 
             this.inputStream = inputStream;
-            this.outputStream = outputStream;
-            if (inputStream is not null)
-            {
-                this.inputStream = inputStream;
-                this.inputReader = new BinaryReader(inputStream);
-            }
+            this.inputReader = new BinaryReader(inputStream);
 
-            if (outputStream is not null)
-            {
-                this.outputStream = outputStream;
-                this.outputWriter = new BinaryWriter(outputStream);
-            }
+            this.outputStream = outputStream;
+            this.outputWriter = new BinaryWriter(this.outputStream);
 
             this.data = new VMData(this);
         }
@@ -181,9 +205,9 @@ namespace AdventOfCode.Intcode
                 this.pointer = DEFAULT;
                 this.originalState.CopyTo(this.memory);
 
-                this.inputStream?.Seek(0L, SeekOrigin.Begin);
-                this.outputStream?.Seek(0L, SeekOrigin.Begin);
-                this.outputStream?.SetLength(0L);
+                this.inputStream.Seek(0L, SeekOrigin.Begin);
+                this.outputStream.Seek(0L, SeekOrigin.Begin);
+                this.outputStream.SetLength(0L);
             }
         }
 
@@ -193,7 +217,7 @@ namespace AdventOfCode.Intcode
         /// <returns>A new copy of the current output array of the VM</returns>
         public int[] GetOutput()
         {
-            if (this.outputStream is null) return Array.Empty<int>();
+            if (this.outputStream.Length is 0) return Array.Empty<int>();
             
             byte[] serialized = this.outputStream.ToArray();
             int[] output = new int[serialized.Length / sizeof(int)];
@@ -206,7 +230,7 @@ namespace AdventOfCode.Intcode
         /// </summary>
         /// <returns>The next integer in the input stream</returns>
         /// <exception cref="InvalidOperationException">If no input stream is specified</exception>
-        private int GetNextInput() => this.inputReader?.ReadInt32() ?? throw new InvalidOperationException("Cannot get input, no input stream set.");
+        private int GetNextInput() => this.inputReader.ReadInt32();
 
         /// <summary>
         /// Adds an integer to the output stream
@@ -224,25 +248,46 @@ namespace AdventOfCode.Intcode
         /// Disposes the IntcodeVM and releases all resources
         /// </summary>
         /// <param name="closeStreams">If the underlying input and output streams should also be closed</param>
-        public void Dispose(bool closeStreams = false)
+        public void Dispose(bool closeStreams)
         {
             if (!this.IsDisposed)
             {
                 if (closeStreams)
                 {
-                    this.inputStream?.Close();
-                    this.outputStream?.Close();
+                    this.inputStream.Close();
+                    this.outputStream.Close();
                 }
                 
-                this.inputReader?.Dispose();
-                this.outputWriter?.Dispose();
-
-                this.IsDisposed = true;
+                Dispose();
             }
         }
 
         /// <inheritdoc cref="IDisposable.Dispose"/>
-        void IDisposable.Dispose() => Dispose();
+        public void Dispose()
+        {
+            if (!this.IsDisposed)
+            {
+                this.inputReader.Dispose();
+                this.outputWriter.Dispose();
+                GC.SuppressFinalize(this);
+
+                this.IsDisposed = true;
+            }
+        }
+        #endregion
+        
+        #region Static methods
+        /// <summary>
+        /// Creates and returns the buffer for a given array
+        /// </summary>
+        /// <param name="array">Array to create the buffer for</param>
+        /// <returns>Buffer of the array</returns>
+        public static byte[] GetBuffer(int[] array)
+        {
+            byte[] buffer = new byte[array.Length * sizeof(int)];
+            Buffer.BlockCopy(array, 0, buffer, 0, buffer.Length);
+            return buffer;
+        }
         #endregion
     }
 }
