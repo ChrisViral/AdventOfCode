426ea31ad9cba5aaae08f472b39872e0b725fbc5
Christophe Savard <christophe_savard@hotmail.ca>
2021-12-03 01:18:52 -0500

    Add 2021 Day03

---
+++ CSharp/Solvers/AoC2021/Day03.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Solvers.AoC2021;
+
+/// <summary>
+/// Solver for 2021 Day 03
+/// </summary>
+public class Day03 : Solver<string[]>
+{
+    #region Constants
+    /// <summary>Mask for Epsilon and Gamma (only twelve binary digits used)</summary>
+    private const int MASK = 0xFFF;
+    #endregion
+
+    #region Constructors
+    /// <summary>
+    /// Creates a new <see cref="Day03"/> Solver for 2021 - 03 with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/>[] fails</exception>
+    public Day03(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
+    {
+        // Find the count on each binary digit
+        int[] counts = new int[Data[0].Length];
+        foreach (string report in Data)
+        {
+            foreach (int i in ..report.Length)
+            {
+                counts[i] += report[i] is '1' ? 1 : -1;
+            }
+        }
+
+        // Create gama from the data we have
+        int gamma = 0;
+        foreach (int i in ..counts.Length)
+        {
+            gamma <<= 1;
+            if (counts[i] > 0)
+            {
+                gamma |= 1;
+            }
+        }
+
+        // Get epsilon from Gamma
+        int epsilon = ~gamma & MASK;
+        AoCUtils.LogPart1(gamma * epsilon);
+
+        // Create a copy of the counts
+        int[] countsCopy = new int[counts.Length];
+        counts.CopyTo(countsCopy, 0);
+        // Get oxygen generator and CO2 scrubber values
+        int generator = System.Convert.ToInt32(GetRating(counts,     '1', '0'), 2);
+        int scrubber  = System.Convert.ToInt32(GetRating(countsCopy, '0', '1'), 2);
+        AoCUtils.LogPart2(generator * scrubber);
+    }
+
+    /// <summary>
+    /// Gets the rating value while filtering with the given positive and negative characters
+    /// </summary>
+    /// <param name="counts">Pop count for each binary digit</param>
+    /// <param name="positive">Character to keep when positive</param>
+    /// <param name="negative">Character to keep when negative</param>
+    /// <returns>The resulting report</returns>
+    private string GetRating(IList<int> counts, char positive, char negative)
+    {
+        HashSet<string> valid = new(Data);
+        HashSet<string> invalid = new();
+        foreach (int i in ..counts.Count)
+        {
+            // Find value to discard from the working set
+            char toDiscard = counts[i] >= 0 ? negative : positive;
+            foreach (string report in valid.Where(r => r[i] == toDiscard))
+            {
+                // Add to discard set
+                invalid.Add(report);
+                foreach (int j in ..report.Length)
+                {
+                    // Remove count
+                    counts[j] -= report[j] is '1' ? 1 : -1;
+                }
+            }
+
+            valid.ExceptWith(invalid);
+            invalid.Clear();
+
+            if (valid.Count == 1)
+            {
+                break;
+            }
+        }
+
+        return valid.First();
+    }
+
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override string[] Convert(string[] rawInput) => rawInput;
+    #endregion
+}
