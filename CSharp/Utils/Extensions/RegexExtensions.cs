557623d6aca9321b088b74f13fbe3f4507b66d54
Christophe Savard <christophe_savard@hotmail.ca>
2021-12-04 17:44:21 -0500

    Split extensions into files

---
+++ CSharp/Utils/Extensions/RegexExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Utils.Extensions;
+
+/// <summary>
+/// Regex extension methods
+/// </summary>
+public static class RegexExtensions
+{
+    #region Extension methods
+    /// <summary>
+    /// Gets all the captured groups of the match
+    /// </summary>
+    /// <param name="match">Match to get the groups from</param>
+    /// <returns>Enumerable of the captured groups</returns>
+    public static IEnumerable<Group> GetCapturedGroups(this Match match) => match.Groups
+                                                                                 .Cast<Group>()
+                                                                                 .Skip(1)
+                                                                                 .Where(g => !string.IsNullOrEmpty(g.Value));
+    #endregion
+}
