9aa3287e6c9fba6c28e2e7870a7540bf98dc7412
Christophe Savard <christophe_savard@hotmail.ca>
2021-12-05 02:23:58 -0500

    Add 20201 Day05

--- CSharp/Utils/Extensions/RangeExtensions.cs
+++ CSharp/Utils/Extensions/RangeExtensions.cs
@@ -11,17 +11,27 @@ public static class RangeExtensions
 {
     #region Extension methods
     /// <summary>
-    /// Transforms the range into an enumerable over it's start and end
+    /// Transforms the range into an enumerable from <see cref="Range.Start"/> to <see cref="Range.End"/><br/>
+    /// If <see cref="Range.End"/> is less than <see cref="Range.Start"/>, then it enumerates from <see cref="Range.End"/> to <see cref="Range.Start"/> instead<br/>
+    /// If <see cref="Range.Start"/> is marked as <see cref="Index.IsFromEnd"/>, then the first value is excluded<br/>
+    /// If <see cref="Range.End"/> is marked as <see cref="Index.IsFromEnd"/>, then the last value is included
     /// </summary>
     /// <param name="range">Range to convert to enumerable</param>
-    /// <returns>An enumerable over the entire Range</returns>
-    /// <exception cref="ArgumentException">If any of the indices are marked as from the end</exception>
+    /// <returns>An enumerable over the specified range</returns>
     public static IEnumerable<int> AsEnumerable(this Range range)
     {
-        //Make sure the indices aren't from the end
-        if (range.Start.IsFromEnd || range.End.IsFromEnd) throw new ArgumentException("Range indices cannot be from end for enumeration", nameof(range));
+        (Index start, Index end) = (range.Start, range.End);
+        if (start.Value > end.Value)
+        {
+            (start, end) = (end, start);
+        }
 
-        return range.End.Value > range.Start.Value ? Enumerable.Range(range.Start.Value, range.End.Value - range.Start.Value) : Enumerable.Range(0, 0);
+        int startValue = start.Value;
+        int length = end.Value - start.Value;
+        if (range.Start.IsFromEnd) startValue++;
+        if (range.End.IsFromEnd)   length++;
+
+        return Enumerable.Range(startValue, length);
     }
 
     /// <summary>
