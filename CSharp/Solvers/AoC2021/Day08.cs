86185b433896245269c8991248a1779a155eeaf2
Christophe Savard <christophe_savard@hotmail.ca>
2021-12-11 04:33:43 -0500

    Clean up day 8

--- CSharp/Solvers/AoC2021/Day08.cs
+++ CSharp/Solvers/AoC2021/Day08.cs
@@ -34,6 +34,7 @@ public class Day08 : ArraySolver<(string[] signals, string[] outputs)>
 
         long total = 0L;
         HashSet<char>[] values = new HashSet<char>[10];
+        values.Fill(() => new(7));
         foreach ((string[] signals, string[] outputs) in this.Data)
         {
             // Simple values
@@ -48,13 +49,15 @@ public class Day08 : ArraySolver<(string[] signals, string[] outputs)>
             values[0] = new(signals.Find(signal => signal.Length is 6 && !values[9].SetEquals(signal) && values[1].IsProperSubsetOf(signal))!);
             values[6] = new(signals.Find(signal => signal.Length is 6 && !values[9].SetEquals(signal) && !values[0].SetEquals(signal))!);
             values[5] = new(signals.Find(signal => signal.Length is 5 && !values[3].SetEquals(signal) && values[9].Count(signal.Contains) is 5)!);
-            //values[2] = new(signals.Find(signal => signal.Length is 5 && !values[3].SetEquals(signal) && signal != values[5])!);
+            values[2] = new(signals.Find(signal => signal.Length is 5 && !values[3].SetEquals(signal) && !values[5].SetEquals(signal))!);
 
-            int final = values.FindIndex(value => value.Count == outputs[0].Length && outputs[0].All(value.Contains)) * 1000;
-            final    += values.FindIndex(value => value.Count == outputs[1].Length && outputs[1].All(value.Contains)) * 100;
-            final    += values.FindIndex(value => value.Count == outputs[2].Length && outputs[2].All(value.Contains)) * 10;
-            final    += values.FindIndex(value => value.Count == outputs[3].Length && outputs[3].All(value.Contains));
+            int final = values.FindIndex(value => value.SetEquals(outputs[0])) * 1000;
+            final    += values.FindIndex(value => value.SetEquals(outputs[1])) * 100;
+            final    += values.FindIndex(value => value.SetEquals(outputs[2])) * 10;
+            final    += values.FindIndex(value => value.SetEquals(outputs[3]));
             total    += final;
+
+            values.ForEach(value => value.Clear());
         }
 
         AoCUtils.LogPart2(total);
