b92603cfbc6684f9afe894b769503d167ff588b3
Christophe Savard <christophe_savard@hotmail.ca>
2021-12-15 01:38:54 -0500

    Add 2021 Day 14

---
+++ CSharp/Solvers/AoC2021/Day14.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+using AdventOfCode.Utils.Extensions;
+
+namespace AdventOfCode.Solvers.AoC2021;
+
+/// <summary>
+/// Solver for 2021 Day 14
+/// </summary>
+public class Day14 : Solver<(string start, Dictionary<(char, char), char> rules)>
+{
+    private const int CYCLES      = 10;
+    private const int LONG_CYCLES = 40;
+
+    #region Constructors
+    /// <summary>
+    /// Creates a new <see cref="Day14"/> Solver for 2021 - 14 with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
+    public Day14(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
+    {
+        // Setup counter dictionaries
+        Dictionary<char, long> counter       = new();
+        Dictionary<(char, char), long> pairs = new();
+
+        // Initialize all possible characters and pairs
+        foreach ((char first, char second) pair in this.Data.rules.Keys)
+        {
+            counter[pair.first]  = 0L;
+            counter[pair.second] = 0L;
+            pairs[pair]          = 0L;
+        }
+
+        // Add the starting amount of characters and pairs
+        char previous = this.Data.start[0];
+        counter[previous]++;
+        foreach (int i in 1..this.Data.start.Length)
+        {
+            char current = this.Data.start[i];
+            counter[current]++;
+            pairs[(previous, current)]++;
+            previous = current;
+        }
+
+        foreach (int _ in ..CYCLES)
+        {
+            GetNextPairs(ref pairs, counter);
+        }
+
+        long diff = counter.Values.Max() - counter.Values.Min();
+        AoCUtils.LogPart1(diff);
+
+        foreach (int _ in CYCLES..LONG_CYCLES)
+        {
+            GetNextPairs(ref pairs, counter);
+        }
+
+        diff = counter.Values.Max() - counter.Values.Min();
+        AoCUtils.LogPart2(diff);
+    }
+
+    private void GetNextPairs(ref Dictionary<(char, char), long> pairs, IDictionary<char, long> counter)
+    {
+        // Copy old pairs data and clear counts
+        Dictionary<(char, char), long> old = pairs;
+        pairs = old.ToDictionary(p => p.Key, _ => 0L);
+        foreach (((char first, char second) pair, long count) in old)
+        {
+            // Add the amount of inserted characters and pairs
+            char insert = this.Data.rules[pair];
+            counter[insert] += count;
+            pairs[(pair.first, insert)]  += count;
+            pairs[(insert, pair.second)] += count;
+        }
+    }
+
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override (string, Dictionary<(char, char), char>) Convert(string[] rawInput)
+    {
+        Dictionary<(char, char), char> rules = new(rawInput.Length - 1);
+        foreach (string line in rawInput[1..])
+        {
+            string[] splits = line.Split(" -> ");
+            rules.Add((splits[0][0], splits[0][1]), splits[1][0]);
+        }
+        return (rawInput[0], rules);
+    }
+    #endregion
+}
