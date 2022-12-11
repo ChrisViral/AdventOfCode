7a4fb23db48a43bf6a02f852424de04d736c63b3
Christophe Savard <christophe_savard@hotmail.ca>
2022-12-11 07:13:53 -0500

    Cleanup Day 9

--- CSharp/Solvers/AoC2022/Day09.cs
+++ CSharp/Solvers/AoC2022/Day09.cs
@@ -17,17 +17,7 @@ public class Day09 : ArraySolver<Vector2<int>>
     private enum Element
     {
         EMPTY   = '.',
-        VISITED = '#',
-        HEAD = 'H',
-        A = '1',
-        B = '2',
-        C = '3',
-        D = '4',
-        E = '5',
-        F = '6',
-        G = '7',
-        H = '8',
-        I = '9'
+        VISITED = '#'
     }
 
     private const int SIZE = 500;
@@ -45,30 +35,16 @@ public class Day09 : ArraySolver<Vector2<int>>
     /// <inheritdoc cref="Solver{T}.Run"/>
     public override void Run()
     {
-        ConsoleView<Element> visited = new(SIZE, SIZE, v => (char)v, defaultValue: Element.EMPTY);
-        SimulateRope(visited, 2);
-        AoCUtils.LogPart1(visited.Count(v => v is Element.VISITED));
-
-        visited = new(SIZE, SIZE, v => (char)v, defaultValue: Element.EMPTY);
-        SimulateRope(visited, 10);
-        AoCUtils.LogPart2(visited.Count(v => v is Element.VISITED));
+        AoCUtils.LogPart1(SimulateRope(2));
+        AoCUtils.LogPart2(SimulateRope(10));
     }
 
     /// <inheritdoc cref="Solver{T}.Convert"/>
-    protected override Vector2<int> ConvertLine(string line)
-    {
-        Vector2<int> vector = Vector2<int>.ParseFromDirection(line);
-        if (vector == Vector2<int>.Zero)
-        {
-            Console.WriteLine(line);
-        }
+    protected override Vector2<int> ConvertLine(string line) => Vector2<int>.ParseFromDirection(line);
 
-        return vector;
-    }
-
-    private void SimulateRope(ConsoleView<Element> visited, int knotCount)
+    private int SimulateRope(int knotCount)
     {
-        ConsoleView<Element> print = new(SIZE, SIZE, v => (char)v, defaultValue: Element.EMPTY);
+        ConsoleView<Element> visited = new(SIZE, SIZE, v => (char)v, defaultValue: Element.EMPTY);
         Vector2<int>[] knots = new Vector2<int>[knotCount];
         visited[knots[^1]] = Element.VISITED;
         foreach (Vector2<int> movement in this.Data)
@@ -78,31 +54,27 @@ public class Day09 : ArraySolver<Vector2<int>>
             foreach (int _ in ..length)
             {
                 knots[0] += direction;
-                foreach (int i in ..(knots.Length - 1))
+                ref Vector2<int> head = ref knots[0];
+                foreach (int i in 1..knots.Length)
                 {
-                    ref Vector2<int> head = ref knots[i];
-                    ref Vector2<int> tail = ref knots[i + 1];
-                    (int x, int y) = head - tail;
-                    if (Math.Abs(x) is 2)
+                    ref Vector2<int> tail = ref knots[i];
+                    tail += (head - tail) switch
                     {
-                        if (Math.Abs(y) is 2)
-                        {
-                            tail += new Vector2<int>(x / 2, y / 2);
-                        }
-                        else
-                        {
-                            tail += new Vector2<int>(x / 2, y);
-                        }
-                    }
-                    else if (Math.Abs(y) is 2)
-                    {
-                        tail += new Vector2<int>(x, y / 2);
-                    }
+                        var diff when Math.Abs(diff.X) is 2
+                                   && Math.Abs(diff.Y) is 2 => diff / 2,
+                        var diff when Math.Abs(diff.X) is 2 => new(diff.X / 2, diff.Y),
+                        var diff when Math.Abs(diff.Y) is 2 => new(diff.X, diff.Y / 2),
+                        _                                   => Vector2<int>.Zero
+                    };
+
+                    head = ref tail;
                 }
 
                 visited[knots[^1]] = Element.VISITED;
             }
         }
+
+        return visited.Count(v => v is Element.VISITED);
     }
     #endregion
 }
