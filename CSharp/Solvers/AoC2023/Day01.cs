f39099dd5cf645f4a3be35c5ecca14690116b4d9
Christophe Savard <christophe_savard@hotmail.ca>
2023-12-01 00:45:02 -0500

    2023 day 1

--- CSharp/Solvers/AoC2023/Day01.cs
+++ CSharp/Solvers/AoC2023/Day01.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Extensions;
 using AdventOfCode.Solvers.Base;
 using AdventOfCode.Utils;
 
@@ -7,35 +11,87 @@ namespace AdventOfCode.Solvers.AoC2023;
 /// <summary>
 /// Solver for 2023 Day 01
 /// </summary>
-public class Day01 : Solver<int>
+public class Day01 : Solver
 {
-    #region Constructors
+    private readonly SearchValues<char> digits = SearchValues.Create("123456789");
 
+    private readonly Dictionary<string, int> numberValues = new()
+    {
+        ["1"]     = 1,
+        ["2"]     = 2,
+        ["3"]     = 3,
+        ["4"]     = 4,
+        ["5"]     = 5,
+        ["6"]     = 6,
+        ["7"]     = 7,
+        ["8"]     = 8,
+        ["9"]     = 9,
+        ["one"]   = 1,
+        ["two"]   = 2,
+        ["three"] = 3,
+        ["four"]  = 4,
+        ["five"]  = 5,
+        ["six"]   = 6,
+        ["seven"] = 7,
+        ["eight"] = 8,
+        ["nine"]  = 9
+    };
+
+    #region Constructors
     /// <summary>
     /// Creates a new <see cref="Day01"/> Solver with the input data properly parsed
     /// </summary>
     /// <param name="input">Puzzle input</param>
-    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="int"/> fails</exception>
     public Day01(string input) : base(input)
     {
     }
-
     #endregion
 
     #region Methods
-
     /// <inheritdoc cref="Solver.Run"/>
     public override void Run()
     {
-        AoCUtils.LogPart1("");
-        AoCUtils.LogPart2("");
+        int total = 0;
+        foreach (ReadOnlySpan<char> value in this.Data)
+        {
+            total += (value[value.IndexOfAny(digits)] - '0') * 10;
+            total += value[value.LastIndexOfAny(digits)] - '0';
+        }
+
+        AoCUtils.LogPart1(total);
+
+        total = this.Data.Sum(GetCalibrationValue);
+        AoCUtils.LogPart2(total);
     }
 
-    /// <inheritdoc cref="Solver{T}.Convert"/>
-    protected override int Convert(string[] rawInput)
+    private int GetCalibrationValue(string data)
     {
-        return default;
-    }
+        ReadOnlySpan<char> value = data;
+        int calibration = 0;
+        bool first = false, last = false;
+        foreach (int i in ..value.Length)
+        {
+            foreach ((string s, int n) in numberValues)
+            {
+                if (!first && value[i..].StartsWith(s))
+                {
+                    calibration += n * 10;
+                    if (last) return calibration;
 
+                    first = true;
+                }
+
+                if (!last && value[..^i].EndsWith(s))
+                {
+                    calibration += n;
+                    if (first) return calibration;
+
+                    last = true;
+                }
+            }
+        }
+
+        return calibration;
+    }
     #endregion
 }
