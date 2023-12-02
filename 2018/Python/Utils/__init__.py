52dc621fa3fe38eaa7c9e22239f353bd588fb775
Christophe Savard <christophe_savard@hotmail.ca>
2023-12-02 13:37:04 -0500

    restructure

--- 2018/Python/Utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .Vector import Vector, Direction
-from .Grid import Grid
--- 2019/Python/Challenges/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .IntcodeComp import IntcodeComp, State
-from .ParameterDict import ParameterDict
-from .Vector import Vector, Direction
-from .Grid import Grid
