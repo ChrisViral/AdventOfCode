f99cd35dc10ca4c2cb9fc87fb2a0d4ee216e00af
Christophe Savard <christophe_savard@hotmail.ca>
2023-12-03 01:27:15 -0500

    Cleanup I guess

--- CSharp/Solvers/AoC2023/Day03.cs
+++ CSharp/Solvers/AoC2023/Day03.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using AdventOfCode.Solvers.Base;
 using AdventOfCode.Solvers.Specialized;
 using AdventOfCode.Utils;
@@ -13,6 +14,7 @@ namespace AdventOfCode.Solvers.AoC2023
     /// </summary>
     public class Day03 : GridSolver<char>
     {
+        private const char EMPTY = '.';
         private const char GEAR = '*';
 
         #region Constructors
@@ -28,57 +30,57 @@ namespace AdventOfCode.Solvers.AoC2023
         /// <inheritdoc cref="Solver.Run"/>
         public override void Run()
         {
-            Dictionary<Vector2<int>, List<int>> gears = new();
-
             int total = 0;
             HashSet<Vector2<int>> explored = new();
+            Dictionary<Vector2<int>, List<int>> gears = new();
             foreach (Vector2<int> pos in Vector2<int>.Enumerate(this.Data.Width, this.Data.Height))
             {
                 char value = this.Data[pos];
-                if (value is '.' || char.IsNumber(value)) continue;
+                if (value is EMPTY || char.IsNumber(value)) continue;
+
+                List<int>? numbers = null;
+                if (value is GEAR)
+                {
+                    numbers = new();
+                    gears[pos] = numbers;
+                }
 
                 foreach (Vector2<int> adjacent in pos.Adjacent(true))
                 {
-                    if (!this.Data.WithinGrid(adjacent) || explored.Contains(adjacent)) continue;
+                    if (explored.Contains(adjacent) || !this.Data.WithinGrid(adjacent)) continue;
 
                     char c = this.Data[adjacent];
                     if (!char.IsNumber(c)) continue;
 
-                    Vector2<int> current;
-                    for (current = adjacent + Vector2<int>.Left; this.Data.WithinGrid(current) && char.IsNumber(this.Data[current]); current += Vector2<int>.Left) { }
+                    Vector2<int> current = adjacent + Vector2<int>.Left;
+                    while (IsValid(current)) current += Vector2<int>.Left;
 
                     int number = 0;
-                    for (current += Vector2<int>.Right; this.Data.WithinGrid(current) && char.IsNumber(this.Data[current]); current += Vector2<int>.Right)
+                    current += Vector2<int>.Right;
+                    do
                     {
                         number *= 10;
                         number += this.Data[current] - '0';
                         explored.Add(current);
+                        current += Vector2<int>.Right;
                     }
+                    while (IsValid(current));
 
                     total += number;
-
-                    if (value is not GEAR) continue;
-
-                    if (!gears.TryGetValue(pos, out List<int>? numbers))
-                    {
-                        numbers = new();
-                        gears.Add(pos, numbers);
-                    }
-
-                    numbers.Add(number);
+                    numbers?.Add(number);
                 }
             }
-
             AoCUtils.LogPart1(total);
 
-            long gearRatio = gears.Values.Where(n => n.Count is 2).Sum(n => (long)n[0] * n[1]);
-
+            long gearRatio = gears.Values.Where(n => n.Count is 2).Sum(n => n[0] * n[1]);
             AoCUtils.LogPart2(gearRatio);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool IsValid(Vector2<int> pos) => this.Data.WithinGrid(pos) && char.IsNumber(this.Data[pos]);
+
         /// <inheritdoc cref="GridSolver{T}.LineConverter"/>
         protected override char[] LineConverter(string line) => line.ToCharArray();
-
         #endregion
     }
 }
---
+++ CSharp/Utils/Ref.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Utils;
+
+public class Ref<T> where T : struct
+{
+    public T Value { get; set; }
+
+    public Ref(T value) => this.Value = value;
+
+    public static implicit operator T(Ref<T> value) => value.Value;
+
+    public override string ToString() => this.Value.ToString()!;
+}
