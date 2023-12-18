f817861ea3f7e699f8db247b29bdf0fbdabd0640
Christophe Savard <christophe_savard@hotmail.ca>
2023-12-18 02:24:57 -0500

    2023 day 18

---
+++ CSharp/Solvers/AoC2023/Day18.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+using AdventOfCode.Vectors;
+using static AdventOfCode.Solvers.AoC2023.Day18;
+
+namespace AdventOfCode.Solvers.AoC2023;
+
+/// <summary>
+/// Solver for 2023 Day 18
+/// </summary>
+public class Day18 : Solver<DigInstruction[]>
+{
+    public readonly struct DigInstruction
+    {
+        public readonly Vector2<int> instruction;
+        public readonly Vector2<long> longInstruction;
+
+        public DigInstruction(char direction, int length, string longLength, char longDir)
+        {
+            this.instruction = DirectionsUtils.Parse(direction).ToVector(length);
+            Direction longDirection = longDir switch
+            {
+                '0' => Direction.RIGHT,
+                '1' => Direction.DOWN,
+                '2' => Direction.LEFT,
+                '3' => Direction.UP,
+                _   => throw new UnreachableException("Unknown direction")
+            };
+            this.longInstruction = longDirection.ToVector(System.Convert.ToInt64(longLength, 16));
+        }
+
+        public override string ToString() => $"{this.instruction} ({this.longInstruction})";
+    }
+
+    private const string INSTRUCTION_PATTERN = @"([UDLR]) (\d+) \(#([0-9a-f]{5})([0-3])\)";
+
+    #region Constructors
+    /// <summary>
+    /// Creates a new <see cref="Day18"/> Solver with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="DigInstruction"/>[] fails</exception>
+    public Day18(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
+    {
+        int area = CalculateShapeSize(this.Data.Select(d => d.instruction));
+        AoCUtils.LogPart1(area);
+
+        long longArea = CalculateShapeSize(this.Data.Select(d => d.longInstruction));
+        AoCUtils.LogPart2(longArea);
+    }
+
+    public T CalculateShapeSize<T>(IEnumerable<Vector2<T>> verticesInstructions) where T : IBinaryInteger<T>, IMinMaxValue<T>
+    {
+        Vector2<T> current = Vector2<T>.Zero;
+        List<Vector2<T>> vertices = new(this.Data.Length + 1) { current };
+
+        T perimeter = T.Zero;
+        foreach (Vector2<T> instruction in verticesInstructions)
+        {
+            current += instruction;
+            vertices.Add(current);
+            perimeter += T.Abs(instruction.X + instruction.Y);
+        }
+
+        T interior = MathUtils.Shoelace(vertices);
+        return MathUtils.Picks(interior, perimeter);
+    }
+
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override DigInstruction[] Convert(string[] rawInput) => RegexFactory<DigInstruction>.ConstructObjects(INSTRUCTION_PATTERN, rawInput, RegexOptions.Compiled);
+    #endregion
+}
--- CSharp/Utils/MathUtils.cs
+++ CSharp/Utils/MathUtils.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
+using AdventOfCode.Vectors;
 
 namespace AdventOfCode.Utils;
 
@@ -126,4 +130,43 @@ public static class MathUtils
     /// <param name="numbers">List of numbers to get the minimum of</param>
     /// <returns>The minimum of all the passed numbers</returns>
     public static T Min<T>(params T[] numbers) where T : INumber<T> => numbers.Aggregate(Min);
+
+    /// <summary>
+    /// Calculates the area of a polygon from its vertices using the Shoelace formula
+    /// </summary>
+    /// <typeparam name="T">Type of integer making up the vertices</typeparam>
+    /// <param name="vertices">List of vertices</param>
+    /// <returns>The total area of the polygon</returns>
+    /// <exception cref="ArgumentOutOfRangeException">If there are not enough vertices to make a proper 2D polygon</exception>
+    public static T Shoelace<T>(IList<Vector2<T>> vertices) where T : IBinaryInteger<T>, IMinMaxValue<T>
+    {
+        // Make sure we have enough vertices
+        if (vertices.Count <= 0) throw new ArgumentOutOfRangeException(nameof(vertices), vertices.Count, "A 2D polygon requires a minimum of 3 vertices");
+
+        // Add the first vertex to the end if not already there
+        if (vertices[0] != vertices[^1])
+        {
+            vertices.Add(vertices[0]);
+        }
+
+        T area = T.Zero;
+        Vector2<T> current = vertices[0];
+        for (int i = 1; i < vertices.Count; i++)
+        {
+            Vector2<T> next = vertices[i];
+            area += (current.X * next.Y) - (next.X * current.Y);
+            current = next;
+        }
+
+        return area / NumberUtils<T>.Two;
+    }
+
+    /// <summary>
+    /// Calculates the area of a polygon using Pick's Theorem
+    /// </summary>
+    /// <typeparam name="T">Type of integer to calculate for</typeparam>
+    /// <param name="interior">Number of interior points</param>
+    /// <param name="border">Number of exterior points</param>
+    /// <returns>The total area of the polygon</returns>
+    public static T Picks<T>(T interior, T border) where T : IBinaryInteger<T> => interior + (border / NumberUtils<T>.Two) + T.One;
 }
