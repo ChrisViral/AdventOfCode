a4c277cc61ee86551512e5bd2cf2add6ddcf7235
Christophe Savard <christophe_savard@hotmail.ca>
2024-12-03 01:44:59 -0500

    change to file scoped namespaces

--- CSharp/Solvers/AoC2024/Day01.cs
+++ CSharp/Solvers/AoC2024/Day01.cs
@@ -8,52 +8,50 @@ using AdventOfCode.Extensions.Ranges;
 using AdventOfCode.Solvers.Base;
 using AdventOfCode.Utils;
 
-namespace AdventOfCode.Solvers.AoC2024
+namespace AdventOfCode.Solvers.AoC2024;
+
+/// <summary>
+/// Solver for 2024 Day 01
+/// </summary>
+public class Day01 : Solver<(int[] leftList, int[] rightList)>
 {
+    private const string PATTERN = @"(\d+)   (\d+)";
+
+    #region Constructors
     /// <summary>
-    /// Solver for 2024 Day 01
+    /// Creates a new <see cref="Day01"/> Solver with the input data properly parsed
     /// </summary>
-    public class Day01 : Solver<(int[] leftList, int[] rightList)>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
+    public Day01(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
     {
-        private const string PATTERN = @"(\d+)   (\d+)";
+        this.Data.leftList.Sort();
+        this.Data.rightList.Sort();
+        int distance = this.Data.leftList.Zip(this.Data.rightList).Sum(d => Math.Abs(d.First - d.Second));
+        AoCUtils.LogPart1(distance);
 
-        #region Constructors
-        /// <summary>
-        /// Creates a new <see cref="Day01"/> Solver with the input data properly parsed
-        /// </summary>
-        /// <param name="input">Puzzle input</param>
-        /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
-        public Day01(string input) : base(input) { }
-        #endregion
-
-        #region Methods
-        /// <inheritdoc cref="Solver.Run"/>
-        public override void Run()
-        {
-            this.Data.leftList.Sort();
-            this.Data.rightList.Sort();
-            int distance = this.Data.leftList.Zip(this.Data.rightList).Sum(d => Math.Abs(d.First - d.Second));
-            AoCUtils.LogPart1(distance);
-
-            Counter<int> left = new(this.Data.leftList);
-            Counter<int> right = new(this.Data.rightList);
-            int similarity = left.Sum<int>(v => v * left[v] * right.GetValueOrDefault(v));
-            AoCUtils.LogPart2(similarity);
-        }
-
-        /// <inheritdoc cref="Solver{T}.Convert"/>
-        protected override (int[] leftList, int[] rightList) Convert(string[] rawInput)
-        {
-            (int left, int right)[] listData = RegexFactory<(int, int)>.ConstructObjects(PATTERN, rawInput, RegexOptions.Compiled);
-            (int[] leftList, int[] rightList) = (new int[listData.Length], new int[listData.Length]);
-            foreach (int i in ..listData.Length)
-            {
-                leftList[i]  = listData[i].left;
-                rightList[i] = listData[i].right;
-            }
-            return (leftList, rightList);
-        }
-        #endregion
+        Counter<int> left = new(this.Data.leftList);
+        Counter<int> right = new(this.Data.rightList);
+        int similarity = left.Sum<int>(v => v * left[v] * right.GetValueOrDefault(v));
+        AoCUtils.LogPart2(similarity);
     }
-}
 
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override (int[] leftList, int[] rightList) Convert(string[] rawInput)
+    {
+        (int left, int right)[] listData = RegexFactory<(int, int)>.ConstructObjects(PATTERN, rawInput, RegexOptions.Compiled);
+        (int[] leftList, int[] rightList) = (new int[listData.Length], new int[listData.Length]);
+        foreach (int i in ..listData.Length)
+        {
+            leftList[i]  = listData[i].left;
+            rightList[i] = listData[i].right;
+        }
+        return (leftList, rightList);
+    }
+    #endregion
+}
