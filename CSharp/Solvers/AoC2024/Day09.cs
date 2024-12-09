16c0ec6bcffa8cc00294f0a907f81897ffa10aac
Christophe Savard <christophe_savard@hotmail.ca>
2024-12-09 09:15:51 -0500

    Full 2024 day 9

--- CSharp/Solvers/AoC2024/Day09.cs
+++ CSharp/Solvers/AoC2024/Day09.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using AdventOfCode.Extensions.Numbers;
 using AdventOfCode.Extensions.Ranges;
 using AdventOfCode.Solvers.Base;
 using AdventOfCode.Utils;
@@ -13,8 +11,6 @@ namespace AdventOfCode.Solvers.AoC2024
     /// </summary>
     public class Day09 : Solver<string>
     {
-        private record struct Block(int Id, int Size, int Gap);
-
         #region Constructors
         /// <summary>
         /// Creates a new <see cref="Day09"/> Solver with the input data properly parsed
@@ -29,24 +25,23 @@ namespace AdventOfCode.Solvers.AoC2024
         public override void Run()
         {
             // Store locally in span
-            Span<int> fileSystem = stackalloc int[this.Data.Length];
-            foreach (int i in ..fileSystem.Length)
+            Span<int> fileChunks = stackalloc int[this.Data.Length + 1];
+            foreach (int i in ..(fileChunks.Length - 1))
             {
-                fileSystem[i] = this.Data[i] - '0';
+                fileChunks[i] = this.Data[i] - '0';
             }
 
             // Setup for checksum calculation
             long checksum = 0L;
-            int headIndex = 0;
-            int tailIndex = fileSystem.Length - 1;
-            int remainingTail = fileSystem[tailIndex];
+            int tailIndex = fileChunks.Length - 2;
+            int remainingTail = fileChunks[tailIndex];
             int blockIndex = 0;
-            int blockId = 0;
-            while (headIndex < tailIndex)
+            int blockId    = 0;
+            for (int headIndex = 0; headIndex < tailIndex; headIndex++)
             {
                 // Checksum the head block
                 blockId = headIndex / 2;
-                for (int blockEnd = blockIndex + fileSystem[headIndex]; blockIndex < blockEnd; blockIndex++)
+                for (int blockEnd = blockIndex + fileChunks[headIndex]; blockIndex < blockEnd; blockIndex++)
                 {
                     checksum += blockId * blockIndex;
                 }
@@ -54,7 +49,7 @@ namespace AdventOfCode.Solvers.AoC2024
                 // Move the tail block to the current gap
                 headIndex++;
                 blockId = tailIndex / 2;
-                for (int blockEnd = blockIndex + fileSystem[headIndex]; blockIndex < blockEnd; blockIndex++)
+                for (int blockEnd = blockIndex + fileChunks[headIndex]; blockIndex < blockEnd; blockIndex++)
                 {
                     checksum += blockId * blockIndex;
                     if (remainingTail > 1)
@@ -73,10 +68,9 @@ namespace AdventOfCode.Solvers.AoC2024
                             remainingTail = 0;
                             break;
                         }
-                        remainingTail = fileSystem[tailIndex];
+                        remainingTail = fileChunks[tailIndex];
                     }
                 }
-                headIndex++;
             }
 
             // Checksum remaining tail block
@@ -84,68 +78,70 @@ namespace AdventOfCode.Solvers.AoC2024
             {
                 checksum += blockId * blockIndex++;
             }
-
             AoCUtils.LogPart1(checksum);
 
-            // Make file system into linked list
-            LinkedList<Block> linkedFileSystem = [];
-            LinkedListNode<Block>[] blockNodes = new LinkedListNode<Block>[(fileSystem.Length + 1) / 2];
-            for (int i = 0; i < fileSystem.Length - 1; i++)
+            // Create chunk ranges and filesystem
+            int chunkIndex = 0;
+            Span<Range> blocks   = stackalloc Range[fileChunks.Length / 2];
+            Span<int> fileSystem = stackalloc int[fileChunks.Sum()];
+            for (int id = 0, i = 0; id < blocks.Length; id++)
             {
-                int id   = i / 2;
-                int size = fileSystem[i++];
-                int gap  = fileSystem[i];
-                blockNodes[id] = linkedFileSystem.AddLast(new Block(id, size, gap));
+                // Make block range
+                int size    = fileChunks[i++];
+                Range block = new(chunkIndex, chunkIndex + size);
+                blocks[id]  = block;
+                chunkIndex  = block.End.Value + fileChunks[i++];
+
+                // Fill filesystem with block
+                fileSystem[block].Fill(id);
             }
-            blockNodes[^1] = linkedFileSystem.AddLast(new Block(blockNodes.Length - 1, fileSystem[^1], 0));
 
-            tailIndex = blockNodes.Length - 1;
-            LinkedListNode<Block> headBlock = linkedFileSystem.First!;
-            HashSet<int> filledBlocks = new(100) { headBlock.ValueRef.Id };
-            for (LinkedListNode<Block> tailBlock = blockNodes[tailIndex]; !filledBlocks.Contains(tailBlock.ValueRef.Id); tailBlock = blockNodes[--tailIndex])
+            // Loop over all blocks backwards
+            int packedIndex = blocks[0].End.Value;
+            for (int i = blocks.Length - 1; blocks[i].Start.Value > packedIndex; i--)
             {
-                // While the head has no gap, move along the list
-                ref Block tail = ref tailBlock.ValueRef;
-
-                // Find where we can insert the current block
-                for (LinkedListNode<Block> insertionTarget = headBlock; insertionTarget.ValueRef.Id != tail.Id; insertionTarget = insertionTarget.Next!)
+                // Check for gaps from the earliest known packed index
+                ref Range block = ref blocks[i];
+                int startIndex = packedIndex;
+                while (startIndex < block.Start.Value)
                 {
-                    ref Block target = ref insertionTarget.ValueRef;
-                    if (target.Gap >= tail.Size)
+                    // Get target span
+                    int endIndex = startIndex + (block.End.Value - block.Start.Value);
+                    Range targetBlock = startIndex..endIndex;
+                    Span<int> target = fileSystem[targetBlock];
+
+                    // Check if all values in target are 0
+                    int firstNonZero = target.FirstOrDefault(c => c is not 0);
+                    if (firstNonZero is 0)
                     {
-                        // Increase gap on the block before where the tail is being removed
-                        ref Block beforeTail = ref tailBlock.Previous!.ValueRef;
-                        beforeTail.Gap += tail.Size + tail.Gap;
+                        // Copy source span to target
+                        Span<int> source = fileSystem[block];
+                        source.CopyTo(target);
 
-                        // Set the gap on the tail to what is left after insertion
-                        tail.Gap   =  target.Gap - tail.Size;
-                        target.Gap =  0;
+                        // Clear old region and set new block range
+                        source.Clear();
+                        block = targetBlock;
 
-                        // Move node to it's new location
-                        linkedFileSystem.Remove(tailBlock);
-                        linkedFileSystem.AddAfter(insertionTarget, tailBlock);
+                        // If new start point was earliest packed index, move it forwards
+                        if (startIndex == packedIndex)
+                        {
+                            packedIndex = endIndex;
+                        }
                         break;
                     }
-                }
 
-                while (headBlock.ValueRef.Gap is 0)
-                {
-                    headBlock = headBlock.Next!;
-                    filledBlocks.Add(headBlock.ValueRef.Id);
+                    // If not, set the next start index from the end of the encroaching block
+                    startIndex = blocks[firstNonZero].End.Value;
                 }
             }
 
-            checksum   = 0L;
-            blockIndex = 0;
-            foreach (Block block in linkedFileSystem)
+            // Calculate checksum
+            checksum = 0L;
+            foreach (int i in ..fileSystem.Length)
             {
-                int endIndex = blockIndex + block.Size;
-                int idSum    = (endIndex - 1).Triangular() - (blockIndex - 1).Triangular();
-                checksum    += idSum * block.Id;
-                blockIndex   = endIndex + block.Gap;
+                checksum += i * fileSystem[i];
             }
-
-            AoCUtils.LogPart2(fileSystem.Sum());
+            AoCUtils.LogPart2(checksum);
         }
 
         /// <inheritdoc cref="Solver{T}.Convert"/>
