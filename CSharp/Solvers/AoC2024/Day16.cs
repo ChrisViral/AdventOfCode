8a31a8f5e376d65381035b0213ff632fe3b5d701
Christophe Savard <christophe_savard@hotmail.ca>
2024-12-20 14:57:04 -0500

    Fix file scoped namespaces again

--- CSharp/Solvers/AoC2024/Day16.cs
+++ CSharp/Solvers/AoC2024/Day16.cs
@@ -10,104 +10,102 @@ using AdventOfCode.Utils;
 using AdventOfCode.Vectors;
 using SpanLinq;
 
-namespace AdventOfCode.Solvers.AoC2024
+namespace AdventOfCode.Solvers.AoC2024;
+
+/// <summary>
+/// Solver for 2024 Day 16
+/// </summary>
+public class Day16 : Solver<(Grid<bool> maze, Vector2<int> start, Vector2<int> end)>
 {
+    private readonly record struct Move(Vector2<int> Position, Direction Direction);
+
+    private static readonly SearchValues<char> Markers = SearchValues.Create('E', 'S');
+
+    #region Constructors
     /// <summary>
-    /// Solver for 2024 Day 16
+    /// Creates a new <see cref="Day16"/> Solver with the input data properly parsed
     /// </summary>
-    public class Day16 : Solver<(Grid<bool> maze, Vector2<int> start, Vector2<int> end)>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
+    public Day16(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
     {
-        private readonly record struct Move(Vector2<int> Position, Direction Direction);
-
-        private static readonly SearchValues<char> Markers = SearchValues.Create('E', 'S');
-
-        #region Constructors
-        /// <summary>
-        /// Creates a new <see cref="Day16"/> Solver with the input data properly parsed
-        /// </summary>
-        /// <param name="input">Puzzle input</param>
-        /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
-        public Day16(string input) : base(input) { }
-        #endregion
-
-        #region Methods
-        /// <inheritdoc cref="Solver.Run"/>
-        public override void Run()
+        Move start = new(this.Data.start, Direction.EAST);
+        Move end   = new(this.Data.end, Direction.NONE);
+        (Move[]? moves, HashSet<Move>? unique) = SearchUtils.SearchEquivalentPaths(start, end,
+                                                                                   null, Neighbours,
+                                                                                   MinSearchComparer<int>.Comparer,
+                                                                                   (c, e) => c.Position == e.Position);
+        int cost = moves!.Length;
+        if (moves[0].Direction is not Direction.EAST)
         {
-            Move start = new(this.Data.start, Direction.EAST);
-            Move end   = new(this.Data.end, Direction.NONE);
-            (Move[]? moves, HashSet<Move>? unique) = SearchUtils.SearchEquivalentPaths(start, end,
-                                                                                       null, Neighbours,
-                                                                                       MinSearchComparer<int>.Comparer,
-                                                                                       (c, e) => c.Position == e.Position);
-            int cost = moves!.Length;
-            if (moves[0].Direction is not Direction.EAST)
+            cost += 1000;
+        }
+        foreach (int i in 1..moves.Length)
+        {
+            if (moves[i].Direction != moves[i - 1].Direction)
             {
                 cost += 1000;
             }
-            foreach (int i in 1..moves.Length)
-            {
-                if (moves[i].Direction != moves[i - 1].Direction)
-                {
-                    cost += 1000;
-                }
-            }
-
-            AoCUtils.LogPart1(cost);
-            AoCUtils.LogPart2(unique!.DistinctBy(m => m.Position).Count());
         }
 
-        private IEnumerable<MoveData<Move, int>> Neighbours(Move move)
-        {
-            Grid<bool> maze = this.Data.maze;
-            Vector2<int> newPosition = move.Position + move.Direction;
-            if (maze[newPosition])
-            {
-                yield return new MoveData<Move, int>(move with { Position = newPosition }, 1);
-            }
-
-            Direction newDirection = move.Direction.TurnRight();
-            newPosition = move.Position + newDirection;
-            if (maze[newPosition])
-            {
-                yield return new MoveData<Move, int>(new Move(newPosition, newDirection), 1001);
-            }
-
-            newDirection = move.Direction.TurnLeft();
-            newPosition = move.Position + newDirection;
-            if (maze[newPosition])
-            {
-                yield return new MoveData<Move, int>(new Move(newPosition, newDirection), 1001);
-            }
-        }
-
-        /// <inheritdoc cref="Solver{T}.Convert"/>
-        protected override (Grid<bool>, Vector2<int>, Vector2<int>) Convert(string[] rawInput)
-        {
-            Grid<bool> maze    = new(rawInput[0].Length, rawInput.Length, rawInput, l => l.AsSpan().Select(c => c is not '#').ToArray(), b => b ? "." : "#");
-            Vector2<int> start = Vector2<int>.Zero;
-            Vector2<int> end   = Vector2<int>.Zero;
-            foreach (int y in ..rawInput.Length)
-            {
-                ReadOnlySpan<char> line = rawInput[y];
-                int x = rawInput[y].AsSpan().IndexOfAny(Markers);
-                if (x is not -1)
-                {
-                    switch (line[x])
-                    {
-                        case 'E':
-                            end = new Vector2<int>(x, y);
-                            break;
-
-                        case 'S':
-                            start = new Vector2<int>(x, y);
-                            break;
-                    }
-                }
-            }
-            return (maze, start, end);
-        }
-        #endregion
+        AoCUtils.LogPart1(cost);
+        AoCUtils.LogPart2(unique!.DistinctBy(m => m.Position).Count());
     }
-}
 
+    private IEnumerable<MoveData<Move, int>> Neighbours(Move move)
+    {
+        Grid<bool> maze = this.Data.maze;
+        Vector2<int> newPosition = move.Position + move.Direction;
+        if (maze[newPosition])
+        {
+            yield return new MoveData<Move, int>(move with { Position = newPosition }, 1);
+        }
+
+        Direction newDirection = move.Direction.TurnRight();
+        newPosition = move.Position + newDirection;
+        if (maze[newPosition])
+        {
+            yield return new MoveData<Move, int>(new Move(newPosition, newDirection), 1001);
+        }
+
+        newDirection = move.Direction.TurnLeft();
+        newPosition  = move.Position + newDirection;
+        if (maze[newPosition])
+        {
+            yield return new MoveData<Move, int>(new Move(newPosition, newDirection), 1001);
+        }
+    }
+
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override (Grid<bool>, Vector2<int>, Vector2<int>) Convert(string[] rawInput)
+    {
+        Grid<bool> maze    = new(rawInput[0].Length, rawInput.Length, rawInput, l => l.AsSpan().Select(c => c is not '#').ToArray(), b => b ? "." : "#");
+        Vector2<int> start = Vector2<int>.Zero;
+        Vector2<int> end   = Vector2<int>.Zero;
+        foreach (int y in ..rawInput.Length)
+        {
+            ReadOnlySpan<char> line = rawInput[y];
+            int x = rawInput[y].AsSpan().IndexOfAny(Markers);
+            if (x is not -1)
+            {
+                switch (line[x])
+                {
+                    case 'E':
+                        end = new Vector2<int>(x, y);
+                        break;
+
+                    case 'S':
+                        start = new Vector2<int>(x, y);
+                        break;
+                }
+            }
+        }
+        return (maze, start, end);
+    }
+    #endregion
+}
--- CSharp/Solvers/AoC2024/Day19.cs
+++ CSharp/Solvers/AoC2024/Day19.cs
@@ -5,98 +5,96 @@ using AdventOfCode.Extensions.Arrays;
 using AdventOfCode.Solvers.Base;
 using AdventOfCode.Utils;
 
-namespace AdventOfCode.Solvers.AoC2024
+namespace AdventOfCode.Solvers.AoC2024;
+
+/// <summary>
+/// Solver for 2024 Day 19
+/// </summary>
+public class Day19 : Solver<(string[] towels, string[] designs)>
 {
+    #region Constructors
     /// <summary>
-    /// Solver for 2024 Day 19
+    /// Creates a new <see cref="Day19"/> Solver with the input data properly parsed
     /// </summary>
-    public class Day19 : Solver<(string[] towels, string[] designs)>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="ValueTuple{T1,T2}"/> fails</exception>
+    public Day19(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
     {
-        #region Constructors
-        /// <summary>
-        /// Creates a new <see cref="Day19"/> Solver with the input data properly parsed
-        /// </summary>
-        /// <param name="input">Puzzle input</param>
-        /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="ValueTuple{T1,T2}"/> fails</exception>
-        public Day19(string input) : base(input) { }
-        #endregion
+        // Keep a set of the valid and invalid designs we found
+        HashSet<string> validDesigns = new(this.Data.designs.Length);
+        HashSet<string> invalidDesigns = new(this.Data.designs.Length);
+        int valid = this.Data.designs.Count(d => TestDesign(d, validDesigns.GetAlternateLookup<ReadOnlySpan<char>>(),
+                                                            invalidDesigns.GetAlternateLookup<ReadOnlySpan<char>>()));
+        AoCUtils.LogPart1(valid);
 
-        #region Methods
-        /// <inheritdoc cref="Solver.Run"/>
-        public override void Run()
+        // Remove designs not part of the original ones
+        validDesigns.IntersectWith(this.Data.designs);
+        // Keep a map of the arrangement count per design
+        Dictionary<string, long> arrangements = new(validDesigns.Count + invalidDesigns.Count);
+        // Add all designs we know are invalid as having 0 arrangements
+        foreach (string invalid in invalidDesigns)
         {
-            // Keep a set of the valid and invalid designs we found
-            HashSet<string> validDesigns = new(this.Data.designs.Length);
-            HashSet<string> invalidDesigns = new(this.Data.designs.Length);
-            int valid = this.Data.designs.Count(d => TestDesign(d, validDesigns.GetAlternateLookup<ReadOnlySpan<char>>(),
-                                                                invalidDesigns.GetAlternateLookup<ReadOnlySpan<char>>()));
-            AoCUtils.LogPart1(valid);
-
-            // Remove designs not part of the original ones
-            validDesigns.IntersectWith(this.Data.designs);
-            // Keep a map of the arrangement count per design
-            Dictionary<string, long> arrangements = new(validDesigns.Count + invalidDesigns.Count);
-            // Add all designs we know are invalid as having 0 arrangements
-            foreach (string invalid in invalidDesigns)
-            {
-                arrangements.Add(invalid, 0L);
-            }
-
-            long possibleDesigns = validDesigns.Sum(d => CountDesigns(d, arrangements.GetAlternateLookup<ReadOnlySpan<char>>()));
-            AoCUtils.LogPart2(possibleDesigns);
+            arrangements.Add(invalid, 0L);
         }
 
-        private bool TestDesign(ReadOnlySpan<char> design,
-                                in HashSet<string>.AlternateLookup<ReadOnlySpan<char>> validDesigns,
-                                in HashSet<string>.AlternateLookup<ReadOnlySpan<char>> invalidDesigns)
-        {
-            if (validDesigns.Contains(design)) return true;
-            if (invalidDesigns.Contains(design)) return false;
-
-            foreach (ReadOnlySpan<char> towel in this.Data.towels)
-            {
-                if (design.StartsWith(towel) && (towel.Length == design.Length || TestDesign(design[towel.Length..], validDesigns, invalidDesigns)))
-                {
-                    validDesigns.Add(design);
-                    return true;
-                }
-            }
-
-            invalidDesigns.Add(design);
-            return false;
-        }
-
-        private long CountDesigns(ReadOnlySpan<char> design, in Dictionary<string, long>.AlternateLookup<ReadOnlySpan<char>> arrangements)
-        {
-            if (arrangements.TryGetValue(design, out long count)) return count;
-
-            foreach (ReadOnlySpan<char> towel in this.Data.towels)
-            {
-                if (!design.StartsWith(towel)) continue;
-
-                if (towel.Length == design.Length)
-                {
-                    count++;
-                }
-                else
-                {
-                    count += CountDesigns(design[towel.Length..], arrangements);
-                }
-            }
-
-            arrangements[design] = count;
-            return count;
-        }
-
-        /// <inheritdoc cref="Solver{T}.Convert"/>
-        protected override (string[], string[]) Convert(string[] rawInput)
-        {
-            string[] towels = rawInput[0].Split(", ");
-            towels.Sort((a, b) => b.Length.CompareTo(a.Length));
-            string[] designs = rawInput[1..];
-            return (towels, designs);
-        }
-        #endregion
+        long possibleDesigns = validDesigns.Sum(d => CountDesigns(d, arrangements.GetAlternateLookup<ReadOnlySpan<char>>()));
+        AoCUtils.LogPart2(possibleDesigns);
     }
-}
 
+    private bool TestDesign(ReadOnlySpan<char> design,
+                            in HashSet<string>.AlternateLookup<ReadOnlySpan<char>> validDesigns,
+                            in HashSet<string>.AlternateLookup<ReadOnlySpan<char>> invalidDesigns)
+    {
+        if (validDesigns.Contains(design)) return true;
+        if (invalidDesigns.Contains(design)) return false;
+
+        foreach (ReadOnlySpan<char> towel in this.Data.towels)
+        {
+            if (design.StartsWith(towel) && (towel.Length == design.Length || TestDesign(design[towel.Length..], validDesigns, invalidDesigns)))
+            {
+                validDesigns.Add(design);
+                return true;
+            }
+        }
+
+        invalidDesigns.Add(design);
+        return false;
+    }
+
+    private long CountDesigns(ReadOnlySpan<char> design, in Dictionary<string, long>.AlternateLookup<ReadOnlySpan<char>> arrangements)
+    {
+        if (arrangements.TryGetValue(design, out long count)) return count;
+
+        foreach (ReadOnlySpan<char> towel in this.Data.towels)
+        {
+            if (!design.StartsWith(towel)) continue;
+
+            if (towel.Length == design.Length)
+            {
+                count++;
+            }
+            else
+            {
+                count += CountDesigns(design[towel.Length..], arrangements);
+            }
+        }
+
+        arrangements[design] = count;
+        return count;
+    }
+
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override (string[], string[]) Convert(string[] rawInput)
+    {
+        string[] towels = rawInput[0].Split(", ");
+        towels.Sort((a, b) => b.Length.CompareTo(a.Length));
+        string[] designs = rawInput[1..];
+        return (towels, designs);
+    }
+    #endregion
+}
