20d0cf202ac52bb2daddc3ba1546e6b8b3be977d
Christophe Savard <christophe_savard@hotmail.ca>
2024-12-22 20:13:52 -0500

    parallelize day 22

--- CSharp/Solvers/AoC2024/Day22.cs
+++ CSharp/Solvers/AoC2024/Day22.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Extensions.Ranges;
 using AdventOfCode.Solvers.Base;
@@ -6,92 +8,115 @@ using AdventOfCode.Solvers.Specialized;
 using AdventOfCode.Utils;
 using AdventOfCode.Vectors;
 
-namespace AdventOfCode.Solvers.AoC2024
+namespace AdventOfCode.Solvers.AoC2024;
+
+/// <summary>
+/// Solver for 2024 Day 22
+/// </summary>
+public class Day22 : ArraySolver<long>
 {
-    /// <summary>
-    /// Solver for 2024 Day 22
-    /// </summary>
-    public class Day22 : ArraySolver<long>
+    private record ParallelData(byte[] SearchValues)
     {
-        /// <summary>
-        /// Daily secret generation count
-        /// </summary>
-        private const int GEN_COUNT = 2000;
+        public int BestValue { get; set; }
+    }
 
-        #region Constructors
-        /// <summary>
-        /// Creates a new <see cref="Day22"/> Solver with the input data properly parsed
-        /// </summary>
-        /// <param name="input">Puzzle input</param>
-        /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
-        public Day22(string input) : base(input) { }
-        #endregion
+    private class ParallelHelper((int[] prices, byte[] diffs)[] secrets) : ParallelHelper<(Vector2<sbyte> a, Vector2<sbyte> b), ParallelData>
+    {
+        public ConcurrentBag<int> Results { get; } = [];
 
-        #region Methods
-        /// <inheritdoc cref="Solver.Run"/>
-        public override void Run()
+        /// <inheritdoc />
+        protected override ParallelData Setup() => new(new byte[4]);
+
+        /// <inheritdoc />
+        protected override void Process((Vector2<sbyte> a, Vector2<sbyte> b) element, in IterationData iteration)
         {
-            (int[] prices, byte[] diffs, long secret)[] secrets = this.Data.Select(GeneratePrices).ToArray();
-            AoCUtils.LogPart1(secrets.Sum(s => s.secret));
-
-            // It ain't efficient, but it takes less than 30s and I cba
-            int bestBananas = 0;
-            Span<byte> search = stackalloc byte[4];
-            foreach (Vector2<sbyte> a in Vector2<sbyte>.Enumerate(19, 19))
+            Span<byte> search = iteration.data.SearchValues;
+            search[0] = (byte)element.a.X;
+            search[1] = (byte)element.a.Y;
+            search[2] = (byte)element.b.X;
+            search[3] = (byte)element.b.Y;
+            int bananas = 0;
+            foreach ((int[] prices, byte[] diffs) in secrets)
             {
-                search[0] = (byte)a.X;
-                search[1] = (byte)a.Y;
-                foreach (Vector2<sbyte> b in Vector2<sbyte>.Enumerate(19, 19))
-                {
-                    search[2] = (byte)b.X;
-                    search[3] = (byte)b.Y;
-                    int bananas = 0;
-                    foreach ((int[] prices, byte[] diffs, _) in secrets)
-                    {
-                        ReadOnlySpan<byte> diffSpan = diffs.AsSpan(1);
-                        int index = diffSpan.IndexOf(search);
-                        if (index is -1) continue;
+                ReadOnlySpan<byte> diffSpan = diffs.AsSpan(1);
+                int index = diffSpan.IndexOf(search);
+                if (index is -1) continue;
 
-                        bananas += prices[index + 4];
-                    }
-
-                    bestBananas = Math.Max(bestBananas, bananas);
-                }
+                bananas += prices[index + 4];
             }
-
-            AoCUtils.LogPart2(bestBananas);
-        }
-
-        private static (int[] prices, byte[] diffs, long secret) GeneratePrices(long seed)
-        {
-            // Create generation arrays
-            int[] prices  = new int[GEN_COUNT + 1];
-            byte[] diffs = new byte[GEN_COUNT + 1];
-
-            int previousPrice = (int)(seed % 10);
-            prices[0]   = previousPrice;
-            long secret = seed;
-            foreach (int i in 1..^GEN_COUNT)
-            {
-                secret ^= secret << 6;  // * 64
-                secret &= 0xFFFFFF;     // % 16777216
-                secret ^= secret >> 5;  // / 32
-                secret &= 0xFFFFFF;     // % 16777216
-                secret ^= secret << 11; // * 2048
-                secret &= 0xFFFFFF;     // % 16777216
-
-                int price     = (int)(secret % 10L);
-                prices[i]     = price;
-                diffs[i]      = (byte)(price - previousPrice + 9);
-                previousPrice = price;
-            }
-
-            return (prices, diffs, secret);
+            iteration.data.BestValue = Math.Max(iteration.data.BestValue, bananas);
         }
 
         /// <inheritdoc />
-        protected override long ConvertLine(string line) => long.Parse(line);
-        #endregion
+        protected override void Finalize(ParallelData data) => Results.Add(data.BestValue);
     }
-}
 
+    /// <summary>
+    /// Daily secret generation count
+    /// </summary>
+    private const int GEN_COUNT = 2000;
+
+    #region Constructors
+    /// <summary>
+    /// Creates a new <see cref="Day22"/> Solver with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
+    public Day22(string input) : base(input) { }
+    #endregion
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
+    {
+        (int[] prices, byte[] diffs, long secret)[] data = this.Data.Select(GeneratePrices).ToArray();
+        AoCUtils.LogPart1(data.Sum(s => s.secret));
+
+        ParallelHelper helper = new(data.Select(d => (d.prices, d.diffs)).ToArray());
+        helper.ForEach(EnumerateSequences());
+        int maxBananas = helper.Results.Max();
+        AoCUtils.LogPart2(maxBananas);
+    }
+
+    private static (int[] prices, byte[] diffs, long secret) GeneratePrices(long seed)
+    {
+        // Create generation arrays
+        int[] prices  = new int[GEN_COUNT + 1];
+        byte[] diffs = new byte[GEN_COUNT + 1];
+
+        int previousPrice = (int)(seed % 10);
+        prices[0]   = previousPrice;
+        long secret = seed;
+        foreach (int i in 1..^GEN_COUNT)
+        {
+            secret ^= secret << 6;  // * 64
+            secret &= 0xFFFFFF;     // % 16777216
+            secret ^= secret >> 5;  // / 32
+            secret &= 0xFFFFFF;     // % 16777216
+            secret ^= secret << 11; // * 2048
+            secret &= 0xFFFFFF;     // % 16777216
+
+            int price     = (int)(secret % 10L);
+            prices[i]     = price;
+            diffs[i]      = (byte)(price - previousPrice + 9);
+            previousPrice = price;
+        }
+
+        return (prices, diffs, secret);
+    }
+
+    private static IEnumerable<(Vector2<sbyte>, Vector2<sbyte>)> EnumerateSequences()
+    {
+        foreach (Vector2<sbyte> a in Vector2<sbyte>.Enumerate(19, 19))
+        {
+            foreach (Vector2<sbyte> b in Vector2<sbyte>.Enumerate(19, 19))
+            {
+                yield return (a, b);
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    protected override long ConvertLine(string line) => long.Parse(line);
+    #endregion
+}
--- CSharp/Utils/Ref.cs
+++ CSharp/Utils/Ref.cs
@@ -16,12 +16,17 @@ public class Ref<T>(T value) where T : struct
     public T Value { get; set; } = value;
 
     /// <summary>
+    /// By-Ref struct wrapper
+    /// </summary>
+    public Ref() : this(default) { }
+
+    /// <inheritdoc cref="object.ToString"/>
+    public override string ToString() => this.Value.ToString()!;
+
+    /// <summary>
     /// Unwraps the struct to it's original value
     /// </summary>
     /// <param name="value">By-ref struct value</param>
     /// <returns>A copy of the struct's value</returns>
     public static implicit operator T(Ref<T> value) => value.Value;
-
-    /// <inheritdoc cref="object.ToString"/>
-    public override string ToString() => this.Value.ToString()!;
 }
