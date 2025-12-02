5649bfd027276e4b115b380ce9d634f3d4bd80e2
Christophe Savard <christophe_savard@hotmail.ca>
2025-12-02 02:06:28 -0500

    2025 day 2

---
+++ CSharp/Solvers/AoC2025/Day02.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using AdventOfCode.Extensions.Numbers;
+using AdventOfCode.Extensions.Ranges;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Solvers.AoC2025;
+
+/// <summary>
+/// Solver for 2025 Day 02
+/// </summary>
+public class Day02 : Solver<(long start, long end)[]>
+{
+    #region Constructors
+    /// <summary>
+    /// Creates a new <see cref="Day02"/> Solver with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
+    public Day02(string input) : base(input) { }
+    #endregion
+
+    private static readonly char[] Buffer = new char[19];
+
+    #region Methods
+    /// <inheritdoc cref="Solver.Run"/>
+    public override void Run()
+    {
+        long invalid = 0L;
+        long invalidRepeated = 0L;
+        foreach ((long start, long end) in this.Data)
+        {
+            for (long id = start; id <= end; id++)
+            {
+                id.TryFormat(Buffer, out int size);
+                ReadOnlySpan<char> value = Buffer.AsSpan(0, size);
+                if (IsInvalid(value))
+                {
+                    invalid         += id;
+                    invalidRepeated += id;
+                }
+                else if (IsInvalidRepeated(value))
+                {
+                    invalidRepeated += id;
+                }
+            }
+        }
+
+        AoCUtils.LogPart1(invalid);
+        AoCUtils.LogPart2(invalidRepeated);
+    }
+
+    private static bool IsInvalid(ReadOnlySpan<char> value)
+    {
+        if (!value.Length.IsEven) return false;
+
+        int middle = value.Length / 2;
+        return value[..middle].SequenceEqual(value[middle..]);
+    }
+
+    private static bool IsInvalidRepeated(ReadOnlySpan<char> value)
+    {
+        if (value.Length is 1) return false;
+
+        int maxChunkSize = value.Length / 2;
+        foreach (int chunkSize in 1..^maxChunkSize)
+        {
+            if (AllChunksEqual(value, chunkSize)) return true;
+        }
+        return false;
+    }
+
+    private static bool AllChunksEqual(ReadOnlySpan<char> value, int chunkSize)
+    {
+        if (!chunkSize.IsFactor(value.Length)) return false;
+
+        ReadOnlySpan<char> firstChunk = value[..chunkSize];
+        for (int chunkStart = chunkSize; chunkStart < value.Length; chunkStart += chunkSize)
+        {
+            if (!value.Slice(chunkStart, chunkSize).SequenceEqual(firstChunk)) return false;
+        }
+        return true;
+    }
+
+    /// <inheritdoc cref="Solver{T}.Convert"/>
+    protected override (long, long)[] Convert(string[] rawInput)
+    {
+        // Split ID ranges
+        ReadOnlySpan<char> data = rawInput[0];
+        int count = data.Count(',') + 1;
+        Span<Range> splits = stackalloc Range[count];
+        data.Split(splits, ',');
+
+        (long, long)[] ranges = new (long, long)[count];
+        Span<Range> rangeSplits = stackalloc Range[2];
+        foreach (int i in ..count)
+        {
+            // Parse individual ranges
+            ReadOnlySpan<char> rangeData = data[splits[i]];
+            rangeData.Split(rangeSplits, '-');
+            long start = long.Parse(rangeData[rangeSplits[0]]);
+            long end   = long.Parse(rangeData[rangeSplits[1]]);
+            ranges[i] = (start, end);
+        }
+        return ranges;
+    }
+    #endregion
+}
