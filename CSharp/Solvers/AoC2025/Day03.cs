ed25e7524ab1d28be65ba328d0d889370af30dac
Christophe Savard <christophe_savard@hotmail.ca>
2025-12-03 00:54:03 -0500

    2025 day 3

---
+++ CSharp/Solvers/AoC2025/Day03.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using AdventOfCode.Extensions.Ranges;
+using AdventOfCode.Solvers.Base;
+using AdventOfCode.Utils;
+using SpanLinq;
+
+namespace AdventOfCode.Solvers.AoC2025;
+
+/// <summary>
+/// Solver for 2025 Day 03
+/// </summary>
+public class Day03 : Solver
+{
+    private const char BEST_BATTERY = '9';
+    private const int PART1_COUNT   = 2;
+    private const long PART1_POW    = 10L;
+    private const int PART2_COUNT   = 12;
+    private const long PART2_POW    = 100_000_000_000L;
+
+    /// <summary>
+    /// Creates a new <see cref="Day03"/> Solver with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to <see cref="string"/> fails</exception>
+    public Day03(string input) : base(input) { }
+
+    /// <inheritdoc cref="Solver.Run"/>
+    /// ReSharper disable once CognitiveComplexity
+    public override void Run()
+    {
+        long joltage = this.Data.Sum(b => GetMaxJoltage(b, PART1_COUNT, PART1_POW));
+        AoCUtils.LogPart1(joltage);
+
+        joltage = this.Data.Sum(b => GetMaxJoltage(b, PART2_COUNT, PART2_POW));
+        AoCUtils.LogPart2(joltage);
+    }
+
+    private static long GetMaxJoltage(ReadOnlySpan<char> bank, int count, long pow)
+    {
+        long joltage = 0L;
+        while (count --> 1)
+        {
+            (char best, int index) = GetBestBattery(bank[..^count]);
+            joltage += (best - '0') * pow;
+
+            pow /= 10L;
+            bank = bank[(index + 1)..];
+        }
+        joltage += GetBestBattery(bank).battery - '0';
+
+        return joltage;
+    }
+
+    private static (char battery, int index) GetBestBattery(ReadOnlySpan<char> bank)
+    {
+        if (bank.Length is 1) return (bank[0], 0);
+
+        char best = bank[0];
+        if (best is BEST_BATTERY) return (best, 0);
+
+        int index = 0;
+        foreach (int i in 1..bank.Length)
+        {
+            char battery = bank[i];
+            if (battery is BEST_BATTERY) return (battery, i);
+
+            if (battery > best)
+            {
+                best  = battery;
+                index = i;
+            }
+        }
+
+        return (best, index);
+    }
+}
