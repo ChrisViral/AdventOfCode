eff5f32bdd2402356615bd2375feb8d448e893cb
Christophe Savard <christophe_savard@hotmail.ca>
2025-12-13 16:41:29 -0500

    2018 day 3

---
+++ AdventOfCode/AoC2018/Day03.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Collections;
+using AdventOfCode.Extensions.Enumerables;
+using AdventOfCode.Maths.Vectors;
+using AdventOfCode.Solvers;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.AoC2018;
+
+/// <summary>
+/// Solver for 2018 Day 03
+/// </summary>
+public sealed partial class Day03 : Solver<Day03.FabricArea[]>
+{
+    [GeneratedRegex(@"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")]
+    private static partial Regex FabricAreaMatcher { get; }
+
+    public sealed class FabricArea(int id, int offsetX, int offsetY, int width, int height)
+    {
+        public int ID { get; } = id;
+
+        public Vector2<int> Offset { get; } = new(offsetX, offsetY);
+
+        public Vector2<int> Dimensions { get; } = new(width, height);
+
+        public void SetClaim(Counter<Vector2<int>> claims)
+        {
+            this.Dimensions.AsEnumerable().ForEach(p => claims[p + this.Offset]++);
+        }
+
+        public bool Overlaps(Counter<Vector2<int>> claims)
+        {
+            return this.Dimensions.AsEnumerable().Any(p => claims[p + this.Offset] > 1);
+        }
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="Day03"/> Solver with the input data properly parsed
+    /// </summary>
+    /// <param name="input">Puzzle input</param>
+    /// <exception cref="InvalidOperationException">Thrown if the conversion to the data type fails</exception>
+    public Day03(string input) : base(input) { }
+
+    /// <inheritdoc />
+    public override void Run()
+    {
+        Counter<Vector2<int>> claims = new(1000);
+        this.Data.ForEach(a => a.SetClaim(claims));
+        int overlaps = claims.Counts.Count(c => c >= 2);
+        AoCUtils.LogPart1(overlaps);
+
+        FabricArea notOverlapping = this.Data.First(a => !a.Overlaps(claims));
+        AoCUtils.LogPart2(notOverlapping.ID);
+    }
+
+    /// <inheritdoc />
+    protected override FabricArea[] Convert(string[] rawInput) => RegexFactory<FabricArea>.ConstructObjects(FabricAreaMatcher, rawInput);
+}
