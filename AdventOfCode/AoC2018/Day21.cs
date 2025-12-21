8f0a907b4ab19e4b1b5bfaf2a600c664ef0a56fa
Christophe Savard <christophe_savard@hotmail.ca>
2025-12-21 01:42:53 -0500

    2018 day 21

--- AdventOfCode/AoC2018/Day21.cs
+++ AdventOfCode/AoC2018/Day21.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode.AoC2018.ElfCode;
+using AdventOfCode.Extensions.Arrays;
 using AdventOfCode.Utils;
 
 namespace AdventOfCode.AoC2018;
@@ -19,20 +20,32 @@ public sealed class Day21 : ElfCodeSolver
     /// ReSharper disable once CognitiveComplexity
     public override void Run()
     {
-        RunVM();
-        AoCUtils.LogPart1("");
-        AoCUtils.LogPart2("");
+        int targetIp = this.Data.Instructions.FindIndex(i => i.Opcode is Opcode.EQRR && (i.A is 0 || i.B is 0));
+        Instruction targetInstruction = this.Data.Instructions[targetIp];
+        int targetRegister = targetInstruction.A is not 0 ? targetInstruction.A : targetInstruction.B;
+
+        List<long> values = new(11000);
+        RunVM(values, targetIp, targetRegister);
+        AoCUtils.LogPart1(values[0]);
+        AoCUtils.LogPart2(values[^1]);
+        AoCUtils.Log(values.Count);
     }
 
-    private void RunVM(int input = 0)
+    private void RunVM(List<long> values, long targetIp, int targetRegister)
     {
         Registers registers = new();
-        ref int ip = ref registers[this.Data.InstructionPointer];
-        registers[0] = input;
+        ref long ip = ref registers[this.Data.InstructionPointer];
         while (ip < this.Data.Instructions.Length)
         {
-            ref Instruction instruction = ref this.Data.Instructions[ip];
-            VirtualMachine.RunInstruction(instruction, ref registers);
+            if (ip == targetIp)
+            {
+                long value = registers[targetRegister];
+                if (values.Contains(value)) return;
+
+                values.Add(value);
+            }
+
+            VirtualMachine.RunInstruction(this.Data.Instructions[(int)ip], ref registers);
             ip++;
         }
     }
