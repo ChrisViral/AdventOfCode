0accf2e55cdcbfbb015cd8553f5439bf3701d52a
Christophe Savard <christophe_savard@hotmail.ca>
2025-12-26 21:04:23 -0500

    Extracting hashing code from day 10

---
+++ AdventOfCode/AoC2017/Common/Knot.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Immutable;
+using AdventOfCode.Utils;
+using AdventOfCode.Utils.Extensions.Ranges;
+using JetBrains.Annotations;
+using SpanLinq;
+
+namespace AdventOfCode.AoC2017.Common;
+
+/// <summary>
+/// Knot hashing utilities
+/// </summary>
+[PublicAPI]
+public static class Knot
+{
+    public const int SIZE       = 256;
+    public const int ITERATIONS = 64;
+    public const int CHUNK_SIZE = 16;
+    public const int CHUNKS     = SIZE / CHUNK_SIZE;
+    public static readonly ImmutableArray<byte> StandardSuffix = [17, 31, 73, 47, 23];
+
+    /// <summary>
+    /// Knot hashes the given data
+    /// </summary>
+    /// <param name="data">Data to hash</param>
+    /// <returns>The hashed value</returns>
+    /// ReSharper disable once CognitiveComplexity
+    public static UInt128 Hash(ReadOnlySpan<char> data)
+    {
+        // Create list
+        Span<byte> list = stackalloc byte[SIZE];
+        foreach (int i in ..SIZE)
+        {
+            list[i] = (byte)i;
+        }
+
+        // Get lengths
+        Span<byte> lengths = stackalloc byte[data.Length + StandardSuffix.Length];
+        foreach (int i in ..data.Length)
+        {
+            lengths[i] = (byte)data[i];
+        }
+        StandardSuffix.CopyTo(lengths[^5..]);
+
+        // Hash
+        int position = 0;
+        int skip     = 0;
+        foreach (int _ in ..ITERATIONS)
+        {
+            HashIteration(ref list, ref position, ref skip, lengths);
+        }
+
+        // Get dense hash
+        UInt128 hash = 0UL;
+        foreach (int chunkIndex in ..CHUNKS)
+        {
+            ReadOnlySpan<byte> chunk = list.Slice(chunkIndex * CHUNK_SIZE, CHUNK_SIZE);
+            byte dense = (byte)chunk.Aggregate(0u, (a, b) => a ^ b);
+            hash <<= 8;
+            hash |= dense;
+        }
+        return hash;
+    }
+
+    /// <summary>
+    /// Single iteration of a knot hash
+    /// </summary>
+    /// <param name="list">Data to hash</param>
+    /// <param name="position">Hash position</param>
+    /// <param name="skip">Hash skip</param>
+    /// <param name="lengths">Lengths span</param>
+    public static void HashIteration(ref Span<byte> list, ref int position, ref int skip, ReadOnlySpan<byte> lengths)
+    {
+        foreach (int length in lengths)
+        {
+            int end = position + length - 1;
+            if (end < SIZE)
+            {
+                MemoryExtensions.Reverse(list[position..(end + 1)]);
+            }
+            else
+            {
+                int swapCounts = length / 2;
+                foreach (int offset in ..swapCounts)
+                {
+                    AoCUtils.Swap(ref list[(position + offset) % SIZE],
+                                  ref list[(end - offset) % SIZE]);
+                }
+            }
+
+            position = (position + length + skip) % SIZE;
+            skip = (skip + 1) % SIZE;
+        }
+    }
+}
--- AdventOfCode/AoC2017/Day10.cs
+++ AdventOfCode/AoC2017/Day10.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Immutable;
+﻿using AdventOfCode.AoC2017.Common;
 using AdventOfCode.Solvers;
 using AdventOfCode.Utils;
 using AdventOfCode.Utils.Extensions.Arrays;
 using AdventOfCode.Utils.Extensions.Ranges;
-using SpanLinq;
 
 namespace AdventOfCode.AoC2017;
 
@@ -12,10 +11,6 @@ namespace AdventOfCode.AoC2017;
 /// </summary>
 public sealed class Day10 : Solver<string>
 {
-    private const int SIZE   = 256;
-    private const int ROUNDS = 64;
-    private static readonly ImmutableArray<byte> StandardSuffix = [17, 31, 73, 47, 23];
-
     /// <summary>
     /// Creates a new <see cref="Day10"/> Solver with the input data properly parsed
     /// </summary>
@@ -30,8 +25,8 @@ public sealed class Day10 : Solver<string>
         // Create hash list
         int position = 0;
         int skip     = 0;
-        Span<byte> list = stackalloc byte[SIZE];
-        foreach (int i in ..SIZE)
+        Span<byte> list = stackalloc byte[Knot.SIZE];
+        foreach (int i in ..Knot.SIZE)
         {
             list[i] = (byte)i;
         }
@@ -40,66 +35,12 @@ public sealed class Day10 : Solver<string>
         byte[] numbers = this.Data.Split(',').ConvertAll(byte.Parse);
 
         // Hash and output
-        KnotHash(ref list, ref position, ref skip, numbers);
+        Knot.HashIteration(ref list, ref position, ref skip, numbers);
         AoCUtils.LogPart1(list[0] * list[1]);
 
-        // Reset hash list
-        position = 0;
-        skip     = 0;
-        foreach (int i in ..SIZE)
-        {
-            list[i] = (byte)i;
-        }
-
-        // Obtain new lengths
-        Span<byte> lengths = stackalloc byte[this.Data.Length + StandardSuffix.Length];
-        foreach (int i in ..this.Data.Length)
-        {
-            lengths[i] = (byte)this.Data[i];
-        }
-        StandardSuffix.CopyTo(lengths[^5..]);
-
-        // Hash
-        foreach (int _ in ..ROUNDS)
-        {
-            KnotHash(ref list, ref position, ref skip, lengths);
-        }
-
-        // Get dense hash
-        int start = 0;
-        Span<char> hash = stackalloc char[32];
-        foreach (byte[] chunk in list.Chunk(16))
-        {
-            byte dense = (byte)chunk.Aggregate(0u, (a, b) => a ^ b);
-            dense.TryFormat(hash[start..], out int _, "x");
-            start += 2;
-        }
-        AoCUtils.LogPart2(hash.ToString());
-    }
-
-    private static void KnotHash(ref Span<byte> list, ref int position, ref int skip, ReadOnlySpan<byte> lengths)
-    {
-        foreach (int length in lengths)
-        {
-            int end = position + length - 1;
-            if (end < SIZE)
-            {
-                MemoryExtensions.Reverse(list[position..(end + 1)]);
-            }
-            else
-            {
-                int swapCounts = length / 2;
-                foreach (int offset in ..swapCounts)
-                {
-                    AoCUtils.Swap(ref list[(position + offset) % SIZE],
-                                  ref list[(end - offset) % SIZE]);
-                }
-            }
-
-            position = (position + length + skip) % SIZE;
-            skip = (skip + 1) % SIZE;
-        }
-
+        // Use full hash function
+        UInt128 hash = Knot.Hash(this.Data);
+        AoCUtils.LogPart2(hash.ToString("x"));
     }
 
     /// <inheritdoc />
